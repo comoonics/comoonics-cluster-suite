68bedf5ec0174742ee7922ae6ca62f832a9dc8dc
mark <mark>
2006-06-14 10:45:51 +0000

    template version

---
+++ lib/ComLog.py
@@ -0,0 +1,15 @@
+"""Comoonics Logging module
+
+here should be some more information about the module, that finds its way inot the onlinedoc
+
+"""
+
+
+# here is some internal information
+# $Id: ComLog.py,v 1.1 2006-06-14 10:45:51 mark Exp $
+#
+
+
+__version__ = "$Revision: 1.1 $"
+# $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/Attic/ComLog.py,v $
+# $log$
