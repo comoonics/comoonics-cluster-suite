66057ed1b22d5dcec8813e7595d0de4a25800720
mark <mark>
2006-06-23 16:16:34 +0000

    added mountpoint functions

--- lib/ComDevice.py
+++ lib/ComDevice.py
@@ -7,11 +7,11 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComDevice.py,v 1.2 2006-06-23 12:01:24 mark Exp $
+# $Id: ComDevice.py,v 1.3 2006-06-23 16:16:34 mark Exp $
 #
 
 
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/Attic/ComDevice.py,v $
 
 import os
@@ -20,13 +20,45 @@ import exceptions
 import ComSystem
 from ComExceptions import *
 from ComDisk import Disk
+import ComUtils
 
 class Device(Disk):
     def __init__(self, device):
         Disk.__init__(self,device)
 
+    def isMounted(self):
+        if self.getMountPoint()[0] != "":
+            return 1
+        return 0
+
+    def getMountPoint(self):
+        """ returns first mountpoint of device ang fstype if mounted
+        raises ComException if its not mounted
+        """
+        if not os.path.isfile("/proc/mounts"):
+            raise ComException("/proc/mounts not found.")
+
+        [ i, o ]=os.popen2("cat /proc/mounts")
+        lines=o.readlines()
+        exp="^" + self.getDevicePath() + " (/.*?) .*"
+        self.getLog().debug(exp)
+        mp=ComUtils.grepInLines(lines, exp)
+        if len(mp) == 0:
+            return ["", ""]
+        exp="^" + self.getDevicePath() + " " + mp[0] + " (.*?) .*"
+        self.getLog().debug(exp)
+        fs=ComUtils.grepInLines(lines, exp)
+        if len(fs) == 0:
+            return ["", ""]
+        return [mp[0], fs[0]]
+
+        
+
 # $Log: ComDevice.py,v $
-# Revision 1.2  2006-06-23 12:01:24  mark
+# Revision 1.3  2006-06-23 16:16:34  mark
+# added mountpoint functions
+#
+# Revision 1.2  2006/06/23 12:01:24  mark
 # moved Log to bottom
 #
 # Revision 1.1  2006/06/23 07:56:24  mark
