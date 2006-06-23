230527c9ed7a7c4965631f23dbeff957fc27fbf2
mark <mark>
2006-06-23 11:50:10 +0000

    moved log to bottom

--- lib/ComLog.py
+++ lib/ComLog.py
@@ -6,19 +6,11 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComLog.py,v 1.4 2006-06-23 07:56:47 mark Exp $
+# $Id: ComLog.py,v 1.5 2006-06-23 11:50:10 mark Exp $
 #
 
-
-__version__ = "$Revision: 1.4 $"
+__version__ = "$Revision: 1.5 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/Attic/ComLog.py,v $
-# $Log: ComLog.py,v $
-# Revision 1.4  2006-06-23 07:56:47  mark
-# added comaptibility to Python 2.3
-#
-# Revision 1.3  2006/06/14 10:51:34  mark
-# added Log Tag
-#
 
 import logging
 import exceptions
@@ -37,3 +29,14 @@ def getLogger(name=""):
     
     return __default_log
 
+
+# $Log: ComLog.py,v $
+# Revision 1.5  2006-06-23 11:50:10  mark
+# moved log to bottom
+#
+# Revision 1.4  2006/06/23 07:56:47  mark
+# added comaptibility to Python 2.3
+#
+# Revision 1.3  2006/06/14 10:51:34  mark
+# added Log Tag
+#
