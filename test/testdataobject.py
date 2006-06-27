708b53d668184b91af4b631c68ce7bfc5d1dcb2c
marc <marc>
2006-06-27 16:06:02 +0000

    added tests for copy and deepcopy

--- test/testdataobject.py
+++ test/testdataobject.py
@@ -18,14 +18,31 @@ node.setAttribute("name", "element1")
 doc.appendChild(node)
 
 obj=DataObject(node, doc)
-obj.option="option2"
+obj.setAttribute("option", "option2")
 obj2=copy.deepcopy(obj)
-obj2.option="option3"
+obj4=copy.copy(obj)
+obj2.setAttribute("option", "option3")
 node=obj2.getDocument().createElement('element1a')
 obj2.getElement().appendChild(node)
 obj3=DataObject(node, doc)
 
+print "Base Object: "
 print obj
+print "Deep Copy of Object: "
 print obj2
+print "Copy of Object: "
+print obj4
+print "Change option =>option2new (obj2, deepcopy)"
+obj2.setAttribute("option", "option2new")
+print "Change option =>option3new (obj4, copy)"
+obj4.setAttribute("option", "option3new")
+print "Base Object: "
+print obj
+print "Deep Copy of Object: "
+print obj2
+print "Copy of Object: "
+print obj4
+
+print "Another object: "
 print obj3
 
