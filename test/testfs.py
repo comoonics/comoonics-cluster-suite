4f6ac809b3d40a41dccfbea4c70dad9033c93182
mark <mark>
2006-06-27 17:03:27 +0000

    added SAX Parser

--- test/testfs.py
+++ test/testfs.py
@@ -7,6 +7,7 @@ sys.path.append("../lib")
 import ComDevice
 import ComUtils
 import ComFileSystem
+import ComDataObject
 from ComFileSystem import *
 
 #sourcedisk=ComDisk.Disk("/dev/sdd")
@@ -17,54 +18,81 @@ from ComFileSystem import *
 import xml.dom
 from xml.dom import EMPTY_NAMESPACE
 from xml.dom.ext import PrettyPrint
+from xml.dom.ext.reader import Sax2
+from xml import xpath
 
-#doc = implementation.createRootNode('file:///article.xml')
-doc = xml.dom.getDOMImplementation().createDocument(None, None, None)
-cs = doc.createElementNS(EMPTY_NAMESPACE, 'copyset')
-dest = doc.createElementNS(EMPTY_NAMESPACE, 'destination')
-dest.setAttributeNS(EMPTY_NAMESPACE, 'type', 'filecopy')
-cs.appendChild(dest)
-device = doc.createElementNS(EMPTY_NAMESPACE, 'device')
-device.setAttributeNS(EMPTY_NAMESPACE, 'type', 'lvm')
-device.setAttributeNS(EMPTY_NAMESPACE, 'path', '/dev/VG_SHAREDROOT/LV_SHAREDROOT')
-dest.appendChild(device)
-lvm = doc.createElementNS(EMPTY_NAMESPACE, 'lvm_config')
-device.appendChild(lvm)
-fs = doc.createElementNS(EMPTY_NAMESPACE, 'filesystem')
-fs.setAttributeNS(EMPTY_NAMESPACE, 'type', 'gfs')
-#node = doc.createElementNS(EMPTY_NAMESPACE, 'fs_config')
-fs.setAttributeNS(EMPTY_NAMESPACE, 'bsize', '4096')
-fs.setAttributeNS(EMPTY_NAMESPACE, 'journals', '8')
-fs.setAttributeNS(EMPTY_NAMESPACE, 'clustername', 'mycluster')
-fs.setAttributeNS(EMPTY_NAMESPACE, 'locktable', 'mylocktable')
-fs.setAttributeNS(EMPTY_NAMESPACE, 'lockproto', 'lock_dlm')
-#fs.appendChild(node)
-device.appendChild(fs)
-mopts=doc.createElementNS(EMPTY_NAMESPACE, 'mount_options')
-mopt=doc.createElementNS(EMPTY_NAMESPACE, 'option')
-mopt.setAttributeNS(EMPTY_NAMESPACE, 'name', 'noatime')
-mopts.appendChild(mopt)
-mopt=doc.createElementNS(EMPTY_NAMESPACE, 'option')
-mopt.setAttributeNS(EMPTY_NAMESPACE, 'value', 'lock_nolock')
-mopt.setAttributeNS(EMPTY_NAMESPACE, 'name', 'lockproto')
-mopts.appendChild(mopt)
-fs.appendChild(mopts)
+XML="""<?xml version="1.0" encoding="UTF-8"?>
+<businesscopy>
+<volume_management>
+  <volume_group name="vg1">
+  <logical_volume name="lv1" size="100M"/>
+  <logical_volume name="lv2" size="200M"/>
+  <physical_volume path="/dev/sdc" name="pv1"/>
+  <physical_volume path="/dev/sda1" name="pv2"/>
+  </volume_group>
+</volume_management>
+<copyset>
+  <source type="filecopy">
+    <filesystem type="gfs"/>
+    <mountpoint path="/cluster/pool/data"/>
+  </source>
+  <destination type="filecopy">
+    <device path="/dev/vg1/lv1">
+      <filesystem bsize="4096" journals="8" clustername="mycluster" lockproto="lock_dlm" locktable="mylocktable" type="gfs"/>
+      <mountpoint path="/mnt/tmp">
+        <option name="noatime"/>
+        <option name="lockproto" value="lock_nolock"/>
+      </mountpoint>
+    </device>
+  </destination>
+</copyset>
+<bootdisk path="/dev/wasauchimmer" loader="grub"/>
+</businesscopy>
+"""
 
-doc.appendChild(cs)
+## Copier:
+## -----------
+## getCopier(copyset)
+## ComCopierLocalFilecopy(Copier)
+## evtl mount
+## cp -ax or rsync -a...
+
+# create Reader object
+reader = Sax2.Reader()
+
+#parse the document
+doc = reader.fromString(XML)
 
 PrettyPrint(doc)
 
-dev=ComDevice.Device("/dev/null")
-gfs=ComFileSystem.getFileSystem(fs)
-gfs.formatDevice(dev)
-print gfs.getOptionsString()
-gfs.mount(dev, "/mnt/")
-gfsneu=copy.deepcopy(gfs) 
+sets = xpath.Evaluate('copyset', doc)
+for i in range(len(sets)):
+    print sets[i]
+    devnode=xpath.Evaluate('destination/device', sets[i])[0]
+    dev=ComDevice.Device(devnode, doc)
+    print dev
+    fsnode=xpath.Evaluate('filesystem', devnode)[0]
+    fs=ComFileSystem.getFileSystem(fsnode, doc)
+    print fs
+    mpnode=xpath.Evaluate('mountpoint', devnode)[0]
+    mp=ComFileSystem.MountPoint(mpnode, doc)
+    print mp
+    fs.formatDevice(dev)
+    fs.mount(dev, mp)
+    fs.umountDev(dev)
+    fs.umountDir(mp)
+    
+
+#gfs=ComFileSystem.getFileSystem(fs)
+#gfs.formatDevice(dev)
+#print gfs.getOptionsString()
+#gfs.mount(dev, "/mnt/")
+#gfsneu=copy.deepcopy(gfs) 
 #fs.labelDevice("neueslabel", device)
-gfs.scanOptions()
-print gfs.getOptionsString()
+#gfs.scanOptions()
+#print gfs.getOptionsString()
 #fs.formatDevice(fsconfig)
 #fs=ComFileSystem.getFileSystem("ext3")
 #fs.formatDevice(device)
-print isinstance(gfsneu, gfsFileSystem)
-print gfsneu.getOptionsString()
+#print isinstance(gfsneu, gfsFileSystem)
+#print gfsneu.getOptionsString()
