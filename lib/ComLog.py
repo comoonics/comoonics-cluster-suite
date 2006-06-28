771cc9747f44ba60593f54e7544c318044f28ce5
marc <marc>
2006-06-28 17:27:26 +0000

    comment out import exceptions

--- lib/ComLog.py
+++ lib/ComLog.py
@@ -6,14 +6,14 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComLog.py,v 1.5 2006-06-23 11:50:10 mark Exp $
+# $Id: ComLog.py,v 1.6 2006-06-28 17:27:26 marc Exp $
 #
 
-__version__ = "$Revision: 1.5 $"
+__version__ = "$Revision: 1.6 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/Attic/ComLog.py,v $
 
 import logging
-import exceptions
+#import exceptions
 
 logging.basicConfig()
 __default_log=logging.getLogger("")
@@ -31,7 +31,10 @@ def getLogger(name=""):
 
 
 # $Log: ComLog.py,v $
-# Revision 1.5  2006-06-23 11:50:10  mark
+# Revision 1.6  2006-06-28 17:27:26  marc
+# comment out import exceptions
+#
+# Revision 1.5  2006/06/23 11:50:10  mark
 # moved log to bottom
 #
 # Revision 1.4  2006/06/23 07:56:47  mark
