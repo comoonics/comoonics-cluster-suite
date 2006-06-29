d6db8649469a36e9242ea421efd6a41252c74d0d
mark <mark>
2006-06-29 10:23:07 +0000

    added xml file support

--- test/testdataobject.py
+++ test/testdataobject.py
@@ -4,45 +4,28 @@ import copy
 import xml.dom
 from xml.dom import EMPTY_NAMESPACE
 from xml.dom.ext import PrettyPrint
-
+from xml.dom.ext.reader import Sax2
+from xml import xpath
 
 sys.path.append("../lib")
 
 
-
 from ComDataObject import *
 
-doc = xml.dom.getDOMImplementation().createDocument(None, None, None)
-node = doc.createElement('element1')
-node.setAttribute("name", "element1")
-doc.appendChild(node)
 
-obj=DataObject(node, doc)
-obj.setAttribute("option", "option2")
-obj2=copy.deepcopy(obj)
-obj4=copy.copy(obj)
-obj2.setAttribute("option", "option3")
-node=obj2.getDocument().createElement('element1a')
-obj2.getElement().appendChild(node)
-obj3=DataObject(node, doc)
 
-print "Base Object: "
+# create Reader object
+reader = Sax2.Reader()
+
+#parse the document
+file=os.fdopen(os.open("./example_config.xml",os.O_RDONLY))
+doc = reader.fromStream(file)
+
+
+print'search for id "rootfs"'
+element=xpath.Evaluate('//*[@refid="bootfs"]', doc)[0]
+
+obj=DataObject(element, doc)
+PrettyPrint(obj.getElement())
 print obj
-print "Deep Copy of Object: "
-print obj2
-print "Copy of Object: "
-print obj4
-print "Change option =>option2new (obj2, deepcopy)"
-obj2.setAttribute("option", "option2new")
-print "Change option =>option3new (obj4, copy)"
-obj4.setAttribute("option", "option3new")
-print "Base Object: "
-print obj
-print "Deep Copy of Object: "
-print obj2
-print "Copy of Object: "
-print obj4
-
-print "Another object: "
-print obj3
 
