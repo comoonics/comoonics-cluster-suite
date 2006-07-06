094bb9220ef5fb0a3e0b9392872d1e69f56fb884
mark <mark>
2006-07-06 11:53:11 +0000

    added journal support

--- lib/ComFilesystemCopyObject.py
+++ lib/ComFilesystemCopyObject.py
@@ -7,11 +7,11 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComFilesystemCopyObject.py,v 1.3 2006-07-03 14:30:06 mark Exp $
+# $Id: ComFilesystemCopyObject.py,v 1.4 2006-07-06 11:53:11 mark Exp $
 #
 
 
-__version__ = "$Revision: 1.3 $"
+__version__ = "$Revision: 1.4 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/Attic/ComFilesystemCopyObject.py,v $
 
 from xml import xpath
@@ -20,14 +20,14 @@ from ComDevice import Device
 import ComFileSystem
 from ComFileSystem import FileSystem
 from ComMountpoint import MountPoint
-from ComCopyObject import CopyObject
+from ComCopyObject import CopyObjectJournaled
 from ComExceptions import *         
 
 
-class FilesystemCopyObject(CopyObject):
+class FilesystemCopyObject(CopyObjectJournaled):
     """ Base Class for all source and destination objects"""
     def __init__(self, element, doc):
-        CopyObject.__init__(self, element, doc)
+        CopyObjectJournaled.__init__(self, element, doc)
         try:
             __device=xpath.Evaluate('device', element)[0]
             self.device=Device(__device, doc)
@@ -44,7 +44,8 @@ class FilesystemCopyObject(CopyObject):
         except Exception:
             raise ComException("mountpoint for copyset not defined")
         self.umountfs=False
-
+        self.addToUndoMap(self.filesystem.__class__.__name__, "mount", "umountDir")
+        
     def getFileSystem(self):
         return self.filesystem
     
@@ -65,27 +66,36 @@ class FilesystemCopyObject(CopyObject):
         # Check for mounted
         if not self.device.isMounted():
             self.filesystem.mount(self.device, self.mountpoint)
-            self.umountfs=True
+            self.journal(self.filesystem, "mount", [self.mountpoint])
+            #self.umountfs=True
         # scan filesystem options
         self.filesystem.scanOptions(self.device, self.mountpoint)
     
     def cleanupSource(self):
-        if self.umountfs:
-            self.filesystem.umountDir(self.mountpoint)
-            self.umountfs=False
+        self.replayJournal()
+        self.commitJournal()
+        #if self.umountfs:
+        #    self.filesystem.umountDir(self.mountpoint)
+        #    self.umountfs=False
     
     def cleanupDest(self):
-        self.filesystem.umountDir(self.mountpoint)
+        #self.filesystem.umountDir(self.mountpoint)
+        self.replayJournal()
+        self.commitJournal()
         
     def prepareAsDest(self):
         # - mkfs
         # TODO add some intelligent checks
         self.filesystem.formatDevice(self.device)
         self.filesystem.mount(self.device, self.mountpoint)
+        self.journal(self.filesystem, "mount", [self.mountpoint])
         
 
 # $Log: ComFilesystemCopyObject.py,v $
-# Revision 1.3  2006-07-03 14:30:06  mark
+# Revision 1.4  2006-07-06 11:53:11  mark
+# added journal support
+#
+# Revision 1.3  2006/07/03 14:30:06  mark
 # added undo
 #
 # Revision 1.2  2006/06/30 08:03:17  mark
