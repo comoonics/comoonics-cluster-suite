ddc1c7d0d5ba585ee06aa6b5cb2894fca54cb76c
mark <mark>
2006-07-06 15:10:32 +0000

    added isMounted(mounpoint) resolves Bug #8

--- lib/ComDevice.py
+++ lib/ComDevice.py
@@ -7,15 +7,16 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComDevice.py,v 1.6 2006-07-03 10:40:06 mark Exp $
+# $Id: ComDevice.py,v 1.7 2006-07-06 15:10:32 mark Exp $
 #
 
 
-__version__ = "$Revision: 1.6 $"
+__version__ = "$Revision: 1.7 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/Attic/ComDevice.py,v $
 
 import os
 import exceptions
+import re
 
 import ComSystem
 from ComExceptions import *
@@ -26,20 +27,23 @@ class Device(Disk):
     def __init__(self, element, doc):
         Disk.__init__(self,element, doc)
 
-    def isMounted(self):
-        if self.scanMountPoint()[0]:
-            return True
+    def isMounted(self, mountpoint=None):
+        __lines = self.getMountList()
+        __exp = "^" + self.getDevicePath() + " "
+        if mountpoint:
+            __exp+=mountpoint.getAttribute("name")
+        self.getLog().debug("is mounted: " + __exp)
+        for __line in __lines:
+           if re.search(__exp, __line):
+                return True 
         return False
+
 
     def scanMountPoint(self):
         """ returns first mountpoint of device and fstype if mounted
         returns None if not mounted
         """
-        if not os.path.isfile("/proc/mounts"):
-            raise ComException("/proc/mounts not found.")
-
-        [ i, o ]=os.popen2("cat /proc/mounts")
-        lines=o.readlines()
+        lines=self.getMountList()
         exp="^" + self.getDevicePath() + " (/.*?) .*"
         self.getLog().debug(exp)
         mp=ComUtils.grepInLines(lines, exp)
@@ -52,10 +56,22 @@ class Device(Disk):
         self.getLog().debug("mountpoint %s filesystem %s", mp[0], fs[0])
         return [mp[0], fs[0]]
 
-        
+    """
+    private methods
+    """
+
+    def getMountList(self):
+        if not os.path.isfile("/proc/mounts"):
+            raise ComException("/proc/mounts not found.")
+
+        [ i, o ]=os.popen2("cat /proc/mounts")
+        return o.readlines()
 
 # $Log: ComDevice.py,v $
-# Revision 1.6  2006-07-03 10:40:06  mark
+# Revision 1.7  2006-07-06 15:10:32  mark
+# added isMounted(mounpoint) resolves Bug #8
+#
+# Revision 1.6  2006/07/03 10:40:06  mark
 # some bugfixes
 #
 # Revision 1.5  2006/06/29 08:16:56  mark
