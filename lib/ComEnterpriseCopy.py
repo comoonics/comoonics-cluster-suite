5be3513687f09779a59106b5762072a190104e49
marc <marc>
2006-07-11 09:25:07 +0000

    added support for command selected copysets and modificationsets

--- lib/ComEnterpriseCopy.py
+++ lib/ComEnterpriseCopy.py
@@ -6,11 +6,11 @@ here should be some more information about the module, that finds its way inot t
 """
 
 # here is some internal information
-# $Id: ComEnterpriseCopy.py,v 1.1 2006-07-07 08:41:27 marc Exp $
+# $Id: ComEnterpriseCopy.py,v 1.2 2006-07-11 09:25:07 marc Exp $
 #
 
 
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/Attic/ComEnterpriseCopy.py,v $
 
 import ComDataObject
@@ -44,33 +44,40 @@ class EnterpriseCopy(ComDataObject.DataObject):
             ms=ComModificationset.getModificationset(emodsets[i], doc)
             self.modificationsets.append(ms)
           
-    def doCopysets(self):
+    def doCopysets(self, name=None):
         for copyset in self.copysets:
-            ComLog.getLogger(self.__logStrLevel__).info("Executing copyset %s(%s:%s)" % (copyset.__class__.__name__, copyset.getAttribute("name", "unknown"), copyset.getAttribute("type")))
-            copyset.doCopy()
+            if not name or name == "all" or name == copyset.getAttribute("name", None):
+                ComLog.getLogger(self.__logStrLevel__).info("Executing copyset %s(%s:%s)" % (copyset.__class__.__name__, copyset.getAttribute("name", "unknown"), copyset.getAttribute("type")))
+                copyset.doCopy()
 
-    def undoCopysets(self):
+    def undoCopysets(self, name=None):
         ComLog.getLogger(self.__logStrLevel__).debug("Copysets: %s " % self.copysets)
         self.copysets.reverse()
         for copyset in self.copysets:
-            ComLog.getLogger(self.__logStrLevel__).info("Undoing copyset %s(%s:%s)" % (copyset.__class__.__name__, copyset.getAttribute("name", "unknown"), copyset.getAttribute("type")))
-            copyset.undoCopy()
+            if not name or name == "all" or name == copyset.getAttribute("name", None):
+                ComLog.getLogger(self.__logStrLevel__).info("Undoing copyset %s(%s:%s)" % (copyset.__class__.__name__, copyset.getAttribute("name", "unknown"), copyset.getAttribute("type")))
+                copyset.undoCopy()
 
-    def doModificationsets(self):
+    def doModificationsets(self, name=None):
         for modset in self.modificationsets:
-            ComLog.getLogger(self.__logStrLevel__).info("Executing modificationset %s(%s:%s)" % (modset.__class__.__name__, modset.getAttribute("name", "unknown"), modset.getAttribute("type")))
-            modset.doModifications()
+            if not name or name == "all" or name == modset.getAttribute("name", None):
+                ComLog.getLogger(self.__logStrLevel__).info("Executing modificationset %s(%s:%s)" % (modset.__class__.__name__, modset.getAttribute("name", "unknown"), modset.getAttribute("type")))
+                modset.doModifications()
           
-    def undoModificationsets(self):
+    def undoModificationsets(self, name=None):
         self.modificationsets.reverse()
         for modset in self.modificationsets:
-            ComLog.getLogger(self.__logStrLevel__).info("Undoing modificationset %s(%s:%s)" % (modset.__class__.__name__, modset.getAttribute("name", "unknown"), modset.getAttribute("type")))
-            modset.undoModifications()
+            if not name or name == "all" or name == modset.getAttribute("name", None):
+                ComLog.getLogger(self.__logStrLevel__).info("Undoing modificationset %s(%s:%s)" % (modset.__class__.__name__, modset.getAttribute("name", "unknown"), modset.getAttribute("type")))
+                modset.undoModifications()
 
 #################################
 # $Log: ComEnterpriseCopy.py,v $
-# Revision 1.1  2006-07-07 08:41:27  marc
-# Business is now Enterprise sonst ändert sich nix.
+# Revision 1.2  2006-07-11 09:25:07  marc
+# added support for command selected copysets and modificationsets
+#
+# Revision 1.1  2006/07/07 08:41:27  marc
+# Business is now Enterprise sonst aendert sich nix.
 #
 # Revision 1.4  2006/07/05 13:06:34  marc
 # support names on every tag.
