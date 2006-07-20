ae115d06da36a0c3c0696e9b391c1cb232d78a64
mark <mark>
2006-07-20 10:25:10 +0000

    added check for update

--- lib/comoonics/enterprisecopy/ComPartitionCopyset.py
+++ lib/comoonics/enterprisecopy/ComPartitionCopyset.py
@@ -7,14 +7,15 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComPartitionCopyset.py,v 1.1 2006-07-19 14:29:15 marc Exp $
+# $Id: ComPartitionCopyset.py,v 1.2 2006-07-20 10:25:10 mark Exp $
 #
 
 
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/enterprisecopy/ComPartitionCopyset.py,v $
 
 import os
+import re
 import xml.dom
 import exceptions
 from xml import xpath
@@ -42,28 +43,48 @@ class PartitionCopyset(CopysetJournaled):
         
     def doCopy(self):
         __tmp=os.tempnam("/tmp")
-        if self.destination.hasPartitionTable():
-            self.destination.savePartitionTable(__tmp)
-            self.journal(self.destination, "savePartitionTable", __tmp)
+        if not self.needPartitionTableUpdate():
+            ComLog.getLogger("Copyset").debug("partition tables are the same. No need to update")
         else:
-            self.journal(self.destination, "noPartitionTable")
-        if self.source.hasPartitionTable():
-            __cmd = self.source.getDumpStdout() 
-            __cmd += " | "
-            __cmd += self.destination.getRestoreStdin(True)
-            __rc, __ret = ComSystem.execLocalStatusOutput(__cmd)
-            ComLog.getLogger("Copyset").debug(__cmd + ": " + __ret)
-            if __rc != 0:
-                raise ComException(__cmd + __ret)
-        else:
-            if not self.destination.deletePartitionTable():
-                raise ComException("Partition table on device %s coud not be deleted", 
-                                   self.destination.getDeviceName())
+            if self.destination.hasPartitionTable():
+                self.destination.savePartitionTable(__tmp)
+                self.journal(self.destination, "savePartitionTable", __tmp)
+            else:
+                self.journal(self.destination, "noPartitionTable")
+            if self.source.hasPartitionTable():
+                __cmd = self.source.getDumpStdout() 
+                __cmd += " | "
+                __cmd += self.destination.getRestoreStdin(True)
+                __rc, __ret = ComSystem.execLocalStatusOutput(__cmd)
+                ComLog.getLogger("Copyset").debug(__cmd + ": " + __ret)
+                if __rc != 0:
+                    raise ComException(__cmd + __ret)
+            else:
+                if not self.destination.deletePartitionTable():
+                    raise ComException("Partition table on device %s coud not be deleted", 
+                                       self.destination.getDeviceName())
     
+    def needPartitionTableUpdate(self):
+        """ compares the partition tables of source and destination
+        returns True if they are not the same """
+        n_table=list()
+        d_table=self.destination.getPartitionTable()
+        s_table=self.source.getPartitionTable()
+        for i in range(len(s_table)):
+            n_table.append(re.sub(self.source.getDeviceName(), \
+                                  self.destination.getDeviceName(), \
+                                  s_table[i]))
+        if d_table == n_table:
+            return False
+        else: 
+            return True
         
 
 # $Log: ComPartitionCopyset.py,v $
-# Revision 1.1  2006-07-19 14:29:15  marc
+# Revision 1.2  2006-07-20 10:25:10  mark
+# added check for update
+#
+# Revision 1.1  2006/07/19 14:29:15  marc
 # removed the filehierarchie
 #
 # Revision 1.4  2006/07/05 12:30:02  mark
