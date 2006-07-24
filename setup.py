ab8e0bc2e32ab2b8c66d01dcdb5b77e6bafb53d7
marc <marc>
2006-07-24 10:03:01 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 # here is some internal information
-# $Id: setup.py,v 1.1 2006-07-19 14:30:34 marc Exp $
+# $Id: setup.py,v 1.2 2006-07-24 10:03:01 marc Exp $
 #
 
 
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/Attic/setup.py,v $
 
 from distutils.core import setup
 
-setup(name='comoonics_cs_py',
+setup(name='comoonics-cs-py',
       version='0.1',
       description='Comoonics Clustersuite utilities and libraries written in Python',
       long_description=
@@ -21,12 +21,14 @@ Comoonics Clustersuite utilities and libraries written in Python
       author_email='grimme@atix.de',
       url='http://www.atix.de/comoonics/',
       package_dir =  { '' : 'lib'},
-      packages=      [ 'comoonics', 'comoonics.enterprisecopy' ],
-      scripts=['bin/com-ec']
+      packages=      [ 'comoonics' ],
      )
 
 #########################
 # $Log: setup.py,v $
-# Revision 1.1  2006-07-19 14:30:34  marc
+# Revision 1.2  2006-07-24 10:03:01  marc
+# *** empty log message ***
+#
+# Revision 1.1  2006/07/19 14:30:34  marc
 # initial revision
 #
