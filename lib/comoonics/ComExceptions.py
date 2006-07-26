538ffdbc6c00cb580f9697ac0cb76136450dde9d
marc <marc>
2006-07-26 10:06:47 +0000

    no logging any more

--- lib/comoonics/ComExceptions.py
+++ lib/comoonics/ComExceptions.py
@@ -7,7 +7,7 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComExceptions.py,v 1.1 2006-07-19 14:29:15 marc Exp $
+# $Id: ComExceptions.py,v 1.2 2006-07-26 10:06:47 marc Exp $
 #
 
 
@@ -21,14 +21,17 @@ import ComLog
 class ComException(Exception):
      def __init__(self, value):
          self.value = value
-         ComLog.getLogger("ComException").exception(value)
+         # ComLog.getLogger("ComException").exception(value)
 
      def __str__(self):
          return repr(self.value)
 
 
 # $Log: ComExceptions.py,v $
-# Revision 1.1  2006-07-19 14:29:15  marc
+# Revision 1.2  2006-07-26 10:06:47  marc
+# no logging any more
+#
+# Revision 1.1  2006/07/19 14:29:15  marc
 # removed the filehierarchie
 #
 # Revision 1.2  2006/06/23 11:52:40  mark
