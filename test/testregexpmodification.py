ec348e1eaf073ff521e47a2377e389ddc6806f59
marc <marc>
2006-09-18 13:58:59 +0000

    testprogramm for regexp modification

---
+++ test/testregexpmodification.py
@@ -0,0 +1,36 @@
+import sys
+import traceback
+
+sys.path.append("../lib")
+
+
+
+import os
+import xml.dom
+from xml.dom import EMPTY_NAMESPACE
+from xml.dom.ext import PrettyPrint
+from xml.dom.ext.reader import Sax2
+from xml import xpath
+
+
+from comoonics import ComUtils
+from comoonics.enterprisecopy import ComRegexpModification
+
+def printDom(doc):
+    PrettyPrint(doc)
+
+# create Reader object
+reader = Sax2.Reader()
+
+#parse the document
+document_file="./example_regexp_mod.xml"
+print "Parsing document %s" %document_file
+file=os.fdopen(os.open(document_file, os.O_RDONLY))
+doc = reader.fromStream(file)
+print "OK"
+element = doc.documentElement
+file = element.getElementsByTagName("file")[0]
+print "Doing regexpmodification"
+regexpmod=ComRegexpModification.RegexpModification(element, doc)
+regexpmod.doRegexpModifications(file, False, sys.stdout)
+print "OK"
