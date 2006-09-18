5a08a149bb9ba253f0509f5e8b86efcb83587360
marc <marc>
2006-09-18 13:58:10 +0000

    added options for Regularexpressions

--- lib/comoonics/enterprisecopy/ComRegexpModification.py
+++ lib/comoonics/enterprisecopy/ComRegexpModification.py
@@ -7,11 +7,11 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComRegexpModification.py,v 1.2 2006-07-21 08:59:09 mark Exp $
+# $Id: ComRegexpModification.py,v 1.3 2006-09-18 13:58:10 marc Exp $
 #
 
 
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/enterprisecopy/ComRegexpModification.py,v $
 
 import exceptions
@@ -29,44 +29,83 @@ CMD_CP="/bin/cp"
 
 class RegexpModification(FileModification):
     SAVESTRING=".regexp"
+    DEFAULT_OPTIONS=0
     """ Regular Expression Modification"""
     def __init__(self, element, doc):
         FileModification.__init__(self, element, doc)
-        
+
     def doModifications(self, file):
         save=True
         if self.hasAttribute("nobackup"):
             if self.getAttribute("nobackup") == "1":
                 save=False
         self.doRegexpModifications(file, save)
-        
-    def doRegexpModifications(self, file, save=True):
+
+    def doRegexpModifications(self, file, save=True, dest=None):
         __search = self.getAttribute("search")
         __replace = self.getAttribute("replace")
+        if self.hasAttribute("options"):
+            __options = self.getREOptions(self.getAttribute("options"))
+        else:
+            __options = self.DEFAULT_OPTIONS
         if save:
             __cmd = list()
             __cmd.append(CMD_CP)
             __cmd.append(file.getAttribute("name"))
-            __cmd.append(file.getAttribute("name")+self.SAVESTRING) 
+            __cmd.append(file.getAttribute("name")+self.SAVESTRING)
             __rc, __ret = ComSystem.execLocalStatusOutput(" ".join(__cmd))
             if __rc:
                 ComLog.getLogger("RegexpModification").error(" ".join(__cmd) + " " + __ret)
             else:
-                ComLog.getLogger("RegexpModification").debug(" ".join(__cmd) + " " + __ret) 
+                ComLog.getLogger("RegexpModification").debug(" ".join(__cmd) + " " + __ret)
         if file.hasAttribute("sourcefile"):
             __source=open(file.getAttribute("sourcefile"))
         else:
             __source=open(file.getAttribute("name"))
         __lines=__source.readlines()
         __source.close()
-        __dest=open(file.getAttribute("name"), 'w')
-        for line in __lines:
-            __dest.write(re.sub(__search, __replace, line))
-        
-        __dest.close()
-            
+        if not dest:
+            __dest=open(file.getAttribute("name"), 'w')
+        else:
+            __dest=dest
+        if __options | re.MULTILINE:
+            __dest.write(re.compile(__search, __options).sub(__replace, "".join(__lines)))
+        else:
+            for line in __lines:
+                __dest.write(re.compile(__search, __options).sub(__replace, line))
+
+        if not dest:
+            __dest.close()
+
+    def getREOptions(self, options):
+        __options=self.DEFAULT_OPTIONS
+        if options:
+            if options.find("i") >= 0:
+                __options=__options|re.IGNORECASE
+            if options.find("m") >= 0:
+                __options=__options|re.MULTILINE
+            if options.find("l") >= 0:
+                __options=__options|re.LOCALE
+            if options.find("x") >= 0:
+                __options=__options|re.VERBOSE
+            if options.find("s") >= 0:
+                __options=__options|re.DOTALL
+            if options.find("u") >= 0:
+                __options=__options|re.UNICODE
+
+        return __options
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
 # $Log: ComRegexpModification.py,v $
-# Revision 1.2  2006-07-21 08:59:09  mark
+# Revision 1.3  2006-09-18 13:58:10  marc
+# added options for Regularexpressions
+#
+# Revision 1.2  2006/07/21 08:59:09  mark
 # added nobackup option
 #
 # Revision 1.1  2006/07/19 14:29:15  marc
