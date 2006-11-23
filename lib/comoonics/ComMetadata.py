72154fe03561a670fa8cca79d9720e9ae7353fc3
marc <marc>
2006-11-23 14:18:49 +0000

    initial revision

---
+++ lib/comoonics/ComMetadata.py
@@ -0,0 +1,75 @@
+""" Comoonics metadata objects
+
+a metadata object represents a metadata element in a comoonics-enterprisecopy xmlfile. It will be resolved by
+the method resolve automatically. That means all known children in the metadata element will be resolved as known.
+
+Until now the only metadata element supports is one with archive children of the following type:
+<metadata>
+   <archive ../>
+   <archive ../>
+</metadata>
+
+These are represented by the class ArchiveMetadata.
+
+"""
+
+
+# here is some internal information
+# $Id: ComMetadata.py,v 1.1 2006-11-23 14:18:49 marc Exp $
+#
+
+__version__ = "$Revision: 1.1 $"
+# $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/Attic/ComMetadata.py,v $
+
+from comoonics.ComDataObject import DataObject
+from comoonics.ComExceptions import ComException
+from comoonics import ComLog
+from xml.dom import Element, Node
+
+class UnsupportedMetadataException(ComException): pass
+
+class Metadata(DataObject):
+    """ The Metadata baseclass """
+    __logStrLevel__="ArchiveMetadata"
+    def __new__(cls, *args, **kwds):
+        if len (args) > 0 and isinstance(args[0], Node):
+            archives=args[0].getElementsByTagName("archive")
+            if len(archives) > 0:
+                cls=ArchiveMetadata
+                ComLog.getLogger(Metadata.__logStrLevel__).debug("Returning new object %s" %(cls))
+                return object.__new__(cls, args, kwds)
+            else:
+                raise UnsupportedMetadataException("Unsupported Metadata type in element " % (args[0].tagName))
+        else:
+            raise UnsupportedMetadataException("Unsupported Metadata type because no domelement given (%u)" %(len(args)))
+
+
+    def __init__(self, element, doc=None):
+        DataObject.__init__(self, element, doc)
+
+    """ resolves this metadata object. This is an abstract method and has to be implemented by the childrenclasses """
+    def resolve(self):
+        pass
+
+
+class ArchiveMetadata(Metadata):
+    """ The Archive Metadata baseclass """
+    __logStrLevel__="ArchiveMetadata"
+    def __init__(self, element, doc=None):
+        print "ArchiveMetadata constructor"
+        Metadata.__init__(self, element, doc)
+
+    def resolve(self):
+#        print "ArchiveMetadata.resolve"
+        from ComArchive import Archive
+        earchive=self.getElement().getElementsByTagName("archive")[0]
+        archive=Archive(earchive, self.getDocument())
+#        print "Created archive: %s" %(archive)
+        element=archive.getNextDOMElement()
+        ComLog.getLogger(self.__logStrLevel__).debug("Found element %s" %(element.tagName))
+        return element
+
+# $Log: ComMetadata.py,v $
+# Revision 1.1  2006-11-23 14:18:49  marc
+# initial revision
+#
---
+++ test/testmetadata.py
@@ -0,0 +1,74 @@
+# here is some internal information
+# $Id: testmetadata.py,v 1.1 2006-11-23 14:21:19 marc Exp $
+#
+
+
+__version__ = "$Revision: 1.1 $"
+# $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/test/testmetadata.py,v $
+
+import sys
+import os
+import xml.dom
+from xml.xpath          import Evaluate
+from xml.dom.ext import PrettyPrint
+from xml.dom.ext.reader import Sax2
+
+sys.path.append("../lib")
+
+from comoonics import ComLVM
+from comoonics import ComSystem
+from comoonics import ComLog
+from comoonics.enterprisecopy import ComCopyset
+from comoonics.enterprisecopy.ComLVMCopyObject import LVMCopyObject
+from comoonics.enterprisecopy.ComLVMCopyset import LVMCopyset
+
+ComSystem.__EXEC_REALLY_DO=""
+
+def line(str=None):
+    print
+    print "--------------------"+str+"---------------------------------"
+
+# create Reader object
+reader = Sax2.Reader(validate=1)
+
+filename="./testmetadata.xml"
+if len(sys.argv) > 1:
+    filename=sys.argv[1]
+
+file=os.fdopen(os.open(filename,os.O_RDONLY))
+doc = reader.fromStream(file)
+
+line("XML Document")
+PrettyPrint(doc)
+
+line("sets of copyset@lvm")
+sets = Evaluate('enterprisecopy/copyset[@type="lvm"]', doc)
+print sets
+for i in range(len(sets)):
+    line("set[%u]" % i)
+    cs=ComCopyset.getCopyset(sets[i], doc)
+    print "Copyset: ", cs
+    print "XML: "
+    PrettyPrint(cs.getElement())
+    line("write to dest: %s" % cs)
+    ComSystem.__EXEC_REALLY_DO="ask"
+    try:
+        cs.doCopy()
+    except Exception, e:
+        ComLog.getLogger("testLVMCopyset").warn("Exception %s caught during copy. Undoing." % e)
+        import traceback
+        traceback.print_exc()
+        cs.undoCopy()
+
+
+##################
+# $Log: testmetadata.py,v $
+# Revision 1.1  2006-11-23 14:21:19  marc
+# initial revision
+#
+# Revision 1.2  2006/07/19 14:29:43  marc
+# changed because of change in fs-hierarchie
+#
+# Revision 1.1  2006/06/30 12:40:14  marc
+# initial revision
+#
