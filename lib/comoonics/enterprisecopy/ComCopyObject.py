60f4dfbfe6ddcdd890093b35021d5ccf3d13d82c
marc <marc>
2006-11-23 14:14:45 +0000

    removed factory method

--- lib/comoonics/enterprisecopy/ComCopyObject.py
+++ lib/comoonics/enterprisecopy/ComCopyObject.py
@@ -6,48 +6,59 @@ here should be some more information about the module, that finds its way inot t
 """
 
 # here is some internal information
-# $Id: ComCopyObject.py,v 1.1 2006-07-19 14:29:15 marc Exp $
+# $Id: ComCopyObject.py,v 1.2 2006-11-23 14:14:45 marc Exp $
 #
 
 
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/enterprisecopy/ComCopyObject.py,v $
 
 from comoonics.ComDataObject import DataObject
 from comoonics.ComExceptions import ComException
 from comoonics.ComJournaled import JournaledObject
-
+from comoonics import ComLog
 
 class UnsupportedCopyObjectException(ComException): pass
 
-def getCopyObject(element, doc):
-    """ Factory function to create Copy Objects"""
-    __type=element.getAttribute("type")
-    if __type == "filesystem":
-        from ComFilesystemCopyObject import FilesystemCopyObject
-        return FilesystemCopyObject(element, doc)
-    elif __type == "lvm":
-        from ComLVMCopyObject import LVMCopyObject
-        return LVMCopyObject(element, doc)
-    else:
-        raise UnsupportedCopyObjectException("Unsupported CopyObject type %s in element " % (__type, element.tagName))
-         
+#def getCopyObject(element, doc):
+#    """ Factory function to create Copy Objects"""
+#    __type=element.getAttribute("type")
+#    print "getCopyObject(%s)" %(element.tagName)
+#    if __type == "filesystem":
+#        from ComFilesystemCopyObject import FilesystemCopyObject
+#        return FilesystemCopyObject(element, doc)
+#    elif __type == "lvm":
+#        from ComLVMCopyObject import LVMCopyObject
+#        return LVMCopyObject(element, doc)
+#    else:
+#        raise UnsupportedCopyObjectException("Unsupported CopyObject type %s in element %s" % (__type, element.tagName))
+#
 
 class CopyObject(DataObject):
     """ Base Class for all source and destination objects"""
     __logStrLevel__ = "CopyObject"
     def __init__(self, element, doc):
         DataObject.__init__(self, element, doc)
-        
+        if element.getAttribute("type") == "backup":
+            """ resolving metadata if available """
+            emetadatas=element.getElementsByTagName("metadata")
+            ComLog.getLogger(self.__logStrLevel__).debug("Found %u metadatas" %(len(emetadatas)))
+            for emetadata in emetadatas:
+                from comoonics.ComMetadata import Metadata
+                metadata=Metadata(emetadata)
+                newmetadata=metadata.resolve()
+#                print "Newchild %s" %(newmetadata)
+                element.replaceChild(newmetadata.cloneNode(True, doc), emetadata)
+
     def prepareAsSource(self):
         pass
-    
+
     def cleanupSource(self):
         pass
-    
+
     def cleanupDest(self):
         pass
-    
+
     def prepareAsDest(self):
         pass
 
@@ -72,7 +83,10 @@ class CopyObjectJournaled(CopyObject, JournaledObject):
         """
         self.replayJournal()
 # $Log: ComCopyObject.py,v $
-# Revision 1.1  2006-07-19 14:29:15  marc
+# Revision 1.2  2006-11-23 14:14:45  marc
+# removed factory method
+#
+# Revision 1.1  2006/07/19 14:29:15  marc
 # removed the filehierarchie
 #
 # Revision 1.3  2006/07/06 11:53:43  mark
