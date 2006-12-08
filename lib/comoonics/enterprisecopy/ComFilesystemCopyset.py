c4a69a245d259d51a4025eac85f6b95b17223822
mark <mark>
2006-12-08 09:39:51 +0000

    added support for generic CopyObject Framework

--- lib/comoonics/enterprisecopy/ComFilesystemCopyset.py
+++ lib/comoonics/enterprisecopy/ComFilesystemCopyset.py
@@ -7,23 +7,26 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComFilesystemCopyset.py,v 1.1 2006-07-19 14:29:15 marc Exp $
+# $Id: ComFilesystemCopyset.py,v 1.2 2006-12-08 09:39:51 mark Exp $
 #
 
 
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/enterprisecopy/ComFilesystemCopyset.py,v $
 
 import xml.dom
 import exceptions
 from xml import xpath
 
-import ComCopyObject
+from ComCopyObject import CopyObject
 from ComCopyset import *
 from comoonics.ComDisk import Disk
 from comoonics.ComExceptions import *
 from comoonics import ComSystem
 from comoonics import ComLog
+
+from ComFilesystemCopyObject import FilesystemCopyObject
+from ComArchiveCopyObject import ArchiveCopyObject
 
 CMD_RSYNC="/usr/bin/rsync"
 
@@ -33,31 +36,37 @@ class FilesystemCopyset(Copyset):
         Copyset.__init__(self, element, doc)
         try:
             __source=xpath.Evaluate('source', element)[0]
-            self.source=ComCopyObject.getCopyObject(__source, doc)
-        except Exception:
+            self.source=CopyObject(__source, doc)
+        except Exception, e:
+            ComLog.getLogger(__logStrLevel__).warning(e)
             raise ComException("source for copyset not defined")
         try:
             __dest=xpath.Evaluate('destination', element)[0]
-            self.destination=ComCopyObject.getCopyObject(__dest, doc)
-        except Exception:
+            self.dest=CopyObject(__dest, doc)
+        except Exception, e:
+        #except None:
+            print ("EXCEPTION: %s\n" %e)
+            ComLog.getLogger(__logStrLevel__).warning(e)
             raise ComException("destination for copyset not defined")
-        
+
     def doCopy(self):
         # do everything
+        #stype=self.source.getAttribute("type")
+        #dtype=self.dest.getAttribute("type")
+
         self.prepareSource()
-        self.copyFsAttributes()
+
+        # Update MetaData
+        self.dest.updateMetaData(self.source.getMetaData())
+
         self.prepareDest()
-        __cmd = self.getCopyCommand()
-        __rc, __ret = ComSystem.execLocalStatusOutput(__cmd)
-        ComLog.getLogger("Copyset").debug("doCopy: "  + __cmd +" "+ __ret) 
+
+        # decide how to copy data
+        self._copyData()
+
         self.postSource()
         self.postDest()
-        if __rc:
-            # TODO
-            # check for specific error codes
-            #raise ComException(__cmd + __ret)
-            ComLog.getLogger("Copyset").warning("doCopy: " + __ret)
-    
+
     def undoCopy(self):
         # simple undo we need to think about that again
         try:
@@ -68,44 +77,86 @@ class FilesystemCopyset(Copyset):
             self.postDest()
         except Exception, e:
             ComLog.getLogger(__logStrLevel__).warning(e)
-        
+
     def prepareSource(self):
         #do things like fsck, mount
         # scan for fsconfig
         self.source.prepareAsSource()
-    
+
     def postSource(self):
         self.source.cleanupSource()
-    
+
     def postDest(self):
-        self.destination.cleanupDest()
-    
+        self.dest.cleanupDest()
+
     def prepareDest(self):
         # do things like mkfs, mount
-        self.destination.prepareAsDest()
+        self.dest.prepareAsDest()
 
-    def copyFsAttributes(self):
-        """ copy all Attributes from source to dest that are not defined
-        in dest
-        """
-        # save dest attributes
-        __attr = self.destination.getFileSystem().getElement().attributes
-        # copy all source fs info to dest
-        self.destination.setFileSystem(self.source.getFileSystem())
-        # restore saved attibutes from dest
-        self.destination.getFileSystem().setAttributes(__attr)
+#    def copyFsAttributes(self):
+#        """ copy all Attributes from source to dest that are not defined
+#        in dest
+#        """
+#        # save dest attributes
+#        __attr = self.destination.getFileSystem().getElement().attributes
+#        # copy all source fs info to dest
+#        self.destination.setFileSystem(self.source.getFileSystem())
+#        # restore saved attibutes from dest
+#        self.destination.getFileSystem().setAttributes(__attr)
 
-    def getCopyCommand(self):
+    def _getFSCopyCommand(self):
         __cmd=CMD_RSYNC
         __cmd+=" -aux --delete "
         __cmd+=self.source.getMountpoint().getAttribute("name")
         __cmd+="/ "
-        __cmd+=self.destination.getMountpoint().getAttribute("name")
+        __cmd+=self.dest.getMountpoint().getAttribute("name")
         __cmd+="/"
         return __cmd
 
+    def _copyData(self):
+
+        # 1. copy fs to fs
+        if isinstance(self.source, FilesystemCopyObject):
+            if isinstance(self.dest, FilesystemCopyObject):
+                __cmd = self._getFSCopyCommand()
+                __rc, __ret = ComSystem.execLocalStatusOutput(__cmd)
+                ComLog.getLogger("Copyset").debug("doCopy: "  + __cmd +" "+ __ret)
+                if __rc:
+                    # TODO
+                    # check for specific error codes
+                    #raise ComException(__cmd + __ret)
+                    ComLog.getLogger("Copyset").warning("doCopy: " + __ret)
+                return __rc
+        # 2. copy fs to archive
+            if isinstance(self.dest, ArchiveCopyObject):
+                try:
+                    archive=self.dest.getDataArchive()
+                    mountpoint=self.source.getMountpoint().getAttribute("name")
+                    archive.createArchive("./", mountpoint)
+                    return True
+                #except Exception, e:
+                except None, e:
+                    ComLog.getLogger("Copyset").error(e)
+                return False
+        # 3. copy archive to fs
+        if isinstance(self.source, ArchiveCopyObject):
+            if isinstance(self.dest, FilesystemCopyObject):
+                try:
+                    archive=self.source.getDataArchive()
+                    mountpoint=self.dest.getMountpoint().getAttribute("name")
+                    archive.extractArchive(mountpoint)
+                    return True
+                except Exception, e:
+                    ComLog.getLogger("Copyset").error(e)
+                return False
+        raise ComException("data copy % to % is not supported" \
+                           % self.source.__name__, self.dest.__name__)
+
 # $Log: ComFilesystemCopyset.py,v $
-# Revision 1.1  2006-07-19 14:29:15  marc
+# Revision 1.2  2006-12-08 09:39:51  mark
+# added support for generic CopyObject Framework
+#
+# Revision 1.1  2006/07/19 14:29:15  marc
 # removed the filehierarchie
 #
 # Revision 1.4  2006/07/06 08:59:07  mark
--- lib/comoonics/enterprisecopy/ComPartitionCopyset.py
+++ lib/comoonics/enterprisecopy/ComPartitionCopyset.py
@@ -1,17 +1,44 @@
 """ Comoonics partition copyset module
 
+Example Configuration:
+<destination>
+    <disk>
+        <partition name="1" type="primary" size="10G">
+            <flag name="boot"/>
+        </partition>
+        <partition name="2" type="primary" size="20%"/>
+        <partition name="3" type="primary" size="REMAIN">
+            <flag name="lvm"/>
+        </partition>
+    </disk>
+</destination>
 
-here should be some more information about the module, that finds its way inot the onlinedoc
-
+supported size attributes:
+    [0-9]+[M,G] size in mega/gigabytes
+    1-100%: size in percentage of the whole disk
+    REMAIN: all remaining space
+    [0-9]+ size in sectors
+supported type attributes:
+    primary
+    extended
+    logical
+supported flag name attributes: Note, that some may not be supported by the disk type
+    boot
+    root
+    swap
+    hidden
+    raid
+    lvm
+    lba
 """
 
 
 # here is some internal information
-# $Id: ComPartitionCopyset.py,v 1.2 2006-07-20 10:25:10 mark Exp $
+# $Id: ComPartitionCopyset.py,v 1.3 2006-12-08 09:42:53 mark Exp $
 #
 
 
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/enterprisecopy/ComPartitionCopyset.py,v $
 
 import os
@@ -21,27 +48,47 @@ import exceptions
 from xml import xpath
 
 from ComCopyset import *
+from ComCopyObject import CopyObject
 from comoonics.ComDisk import Disk
 from comoonics.ComExceptions import *
 from comoonics import ComSystem
 
+__logStrLevel__ = "PartitionCopyset"
 class PartitionCopyset(CopysetJournaled):
     def __init__(self, element, doc):
         CopysetJournaled.__init__(self, element, doc)
         try:
-            __source=xpath.Evaluate('source/disk', element)[0]
-            self.source=Disk(__source, doc)
-        except Exception:
+            __source=xpath.Evaluate('source', element)[0]
+            self.source=CopyObject(__source, doc)
+        #except Exception, e:
+        except None:
+            ComLog.getLogger(__logStrLevel__).warning(e)
             raise ComException("source for copyset not defined")
         try:
-            __dest=xpath.Evaluate('destination/disk', element)[0]
-            self.destination=Disk(__dest, doc)
-        except Exception:
+            __dest=xpath.Evaluate('destination', element)[0]
+            self.dest=CopyObject(__dest, doc)
+        except Exception, e:
+        #except None:
+            print ("EXCEPTION: %s\n" %e)
+            ComLog.getLogger(__logStrLevel__).warning(e)
             raise ComException("destination for copyset not defined")
-        self.addToUndoMap(self.source.__class__.__name__, "savePartitionTable", "restorePartitionTable")
-        self.addToUndoMap(self.source.__class__.__name__, "noPartitionTable", "deletePartitionTable")
-        
+
     def doCopy(self):
+        # do everything
+
+        self.source.prepareAsSource()
+
+        # Update MetaData
+        self.dest.updateMetaData(self.source.getMetaData())
+
+        self.dest.prepareAsDest()
+
+        self.source.cleanupSource()
+        self.dest.cleanupDest()
+
+
+
+    def __doCopy_alt(self):
         __tmp=os.tempnam("/tmp")
         if not self.needPartitionTableUpdate():
             ComLog.getLogger("Copyset").debug("partition tables are the same. No need to update")
@@ -52,7 +99,7 @@ class PartitionCopyset(CopysetJournaled):
             else:
                 self.journal(self.destination, "noPartitionTable")
             if self.source.hasPartitionTable():
-                __cmd = self.source.getDumpStdout() 
+                __cmd = self.source.getDumpStdout()
                 __cmd += " | "
                 __cmd += self.destination.getRestoreStdin(True)
                 __rc, __ret = ComSystem.execLocalStatusOutput(__cmd)
@@ -61,9 +108,10 @@ class PartitionCopyset(CopysetJournaled):
                     raise ComException(__cmd + __ret)
             else:
                 if not self.destination.deletePartitionTable():
-                    raise ComException("Partition table on device %s coud not be deleted", 
+                    raise ComException("Partition table on device %s coud not be deleted",
                                        self.destination.getDeviceName())
-    
+
+
     def needPartitionTableUpdate(self):
         """ compares the partition tables of source and destination
         returns True if they are not the same """
@@ -76,12 +124,15 @@ class PartitionCopyset(CopysetJournaled):
                                   s_table[i]))
         if d_table == n_table:
             return False
-        else: 
+        else:
             return True
-        
+
 
 # $Log: ComPartitionCopyset.py,v $
-# Revision 1.2  2006-07-20 10:25:10  mark
+# Revision 1.3  2006-12-08 09:42:53  mark
+# added support for generic CopyObject Framework
+#
+# Revision 1.2  2006/07/20 10:25:10  mark
 # added check for update
 #
 # Revision 1.1  2006/07/19 14:29:15  marc
