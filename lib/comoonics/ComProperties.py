3931cc15bf2dcf4808bf9f70fc8d8ad2caa65d2a
marc <marc>
2007-02-09 11:34:54 +0000

    bugfixes with empty params and different constructors

--- lib/comoonics/ComProperties.py
+++ lib/comoonics/ComProperties.py
@@ -5,10 +5,10 @@ Implementation of properties as DataObject
 
 __version__= "$Revision $"
 
-# $Id: ComProperties.py,v 1.1 2007-01-15 13:58:09 marc Exp $
+# $Id: ComProperties.py,v 1.2 2007-02-09 11:34:54 marc Exp $
 
 import warnings
-from ComDataObject import DataObject
+from comoonics.ComDataObject import DataObject
 import ComLog
 
 class Property(DataObject):
@@ -23,15 +23,22 @@ class Property(DataObject):
     Public methods
     '''
     def __init__(self, *args):
-        if len(args)==1:
-            super(Property, self).__init__(args[0])
-        elif len(args)==3 and type(args[0])==str and type(args[1])==str:
-            doc=args[3]
+        if len(args)==2 and type(args[0])==str and not type(args[1])==str:
+            doc=args[1]
             element=doc.createElement(Property.TAGNAME)
             super(Property, self).__init__(element, doc)
-            self.setAttribute(args[0], args[1])
+            self.setAttribute(args[0], True)
+        elif len(args)==3 and isinstance(args[0], basestring) and isinstance(args[1], basestring):
+            doc=args[2]
+            element=doc.createElement(Property.TAGNAME)
+            super(Property, self).__init__(element, doc)
+            self.setAttribute("name", args[0])
+            self.setAttribute("value", args[1])
         else:
+            #mylogger.debug("%s, %s" %(type(args[0]), type(args[1])))
             super(Property, self).__init__(*args)
+        if self.hasAttribute("name") and not self.hasAttribute("value"):
+            self.setAttribute("value", True)
 
 class Properties(DataObject):
     TAGNAME="properties"
@@ -56,7 +63,7 @@ class Properties(DataObject):
     def getProperty(self, name, d=None):
         return self.getProperties().get(name, d)
     def setProperty(self, name, value):
-        self.getProperties()[name]=Property(name, value)
+        self.getProperties()[name]=Property(name, value, self.getDocument())
 
     def __getitem__(self, name):
         return self.getProperty(name)
@@ -68,7 +75,7 @@ class Properties(DataObject):
         return self.properties.has_key(name)
     def iter(self):
         for property in self.properties:
-            yield property
+            yield self.properties.get(property)
 
 mylogger=ComLog.getLogger(Properties.__logStrLevel__)
 
@@ -78,6 +85,7 @@ def main():
 <properties>
   <property name="testname1" value="testvalue1"/>
   <property name="testname2" value="testvalue2"/>
+  <property name="testflag"/>
 </properties>
 """
     print "Reading xml.."
@@ -92,12 +100,20 @@ def main():
     print "OK"
     print properties
     property_name="testname2"
-    print "Getting property %s: %s" %(property_name, properties[property_name])
+    print "Getting property %s: %s" %(property_name, properties[property_name].getAttribute("value"))
+    property_name="testflag"
+    print "Getting property %s: %s" %(property_name, properties[property_name].getAttribute("value"))
+    print "Walking through properties"
+    for property in properties.iter():
+        print "%s" %(property)
 
 
 if __name__ == '__main__':
     main()
 # $Log: ComProperties.py,v $
-# Revision 1.1  2007-01-15 13:58:09  marc
+# Revision 1.2  2007-02-09 11:34:54  marc
+# bugfixes with empty params and different constructors
+#
+# Revision 1.1  2007/01/15 13:58:09  marc
 # initial revision
 #
