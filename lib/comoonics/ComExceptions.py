099379240b785978837dcbe5e9cc9060915f48ca
marc <marc>
2007-02-09 11:34:10 +0000

    better handling of empty strings

--- lib/comoonics/ComExceptions.py
+++ lib/comoonics/ComExceptions.py
@@ -7,7 +7,7 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComExceptions.py,v 1.2 2006-07-26 10:06:47 marc Exp $
+# $Id: ComExceptions.py,v 1.3 2007-02-09 11:34:10 marc Exp $
 #
 
 
@@ -19,16 +19,24 @@ import exceptions
 import ComLog
 
 class ComException(Exception):
-     def __init__(self, value):
-         self.value = value
+     def __init__(self, value=""):
+         if value != "":
+             self.value = value
+         else:
+             self.value = self.__class__.__name__
          # ComLog.getLogger("ComException").exception(value)
 
      def __str__(self):
+         if isinstance(self.value, unicode):
+             self.value=self.value.decode()
          return repr(self.value)
 
 
 # $Log: ComExceptions.py,v $
-# Revision 1.2  2006-07-26 10:06:47  marc
+# Revision 1.3  2007-02-09 11:34:10  marc
+# better handling of empty strings
+#
+# Revision 1.2  2006/07/26 10:06:47  marc
 # no logging any more
 #
 # Revision 1.1  2006/07/19 14:29:15  marc
