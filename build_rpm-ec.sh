5cf60ebef7da6bd7f672373716a5f7ea5c86dea9
mark <mark>
2007-02-27 15:56:26 +0000

    Release 12

--- build_rpm-ec.sh
+++ build_rpm-ec.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-SETUP_PY="setup-ec.py"
-RELEASE=10
+# $Id: $
+
+source ./build-lib.sh
+
+RELEASE=12
 REQUIRES="--requires=PyXML,libxslt-python,comoonics-cs-py"
 NOAUTO_REQ="--no-autoreq"
+NAME="comoonics-ec-py"
+VERSION="0.1"
+DESCRIPTION="Comoonics Enterprisecopy utilities and libraries written in Python"
+LONG_DESCRIPTION="
+Comoonics Enterprisecopy utilities and libraries written in Python
+"
+AUTHOR="Marc Grimme"
+AUTHOR_EMAIL="grimme@atix.de"
+URL="http://www.atix.de/comoonics/"
+PACKAGE_DIR='"comoonics.enterprisecopy" : "lib/comoonics/enterprisecopy"'
+PACKAGES='"comoonics.enterprisecopy"'
+SCRIPTS='"bin/com-ec"'
 
-rm MANIFEST
-cp -f ${SETUP_PY} setup.py
-python setup.py -v bdist_rpm --release=${RELEASE} ${REQUIRES} ${NOAUTO_REQ}
+setup
+
+##############
+# $Log: $
+#
