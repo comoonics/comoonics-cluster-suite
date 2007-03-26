1e90241f213d22a3900887edde8222999a3f9a8c
marc <marc>
2007-03-26 08:15:21 +0000

    automatically importing comoonics.backup if available

--- lib/comoonics/__init__.py
+++ lib/comoonics/__init__.py
@@ -4,10 +4,19 @@ Init this package
 
 """
 
-pass
+from exceptions import ImportError
+
+try:
+    import comoonics.backup
+except ImportError:
+#    print "Importerror of comoonics.backup"
+            pass
 
 ########
 # $Log: __init__.py,v $
-# Revision 1.1  2006-07-19 14:29:15  marc
+# Revision 1.2  2007-03-26 08:15:21  marc
+# automatically importing comoonics.backup if available
+#
+# Revision 1.1  2006/07/19 14:29:15  marc
 # removed the filehierarchie
 #
