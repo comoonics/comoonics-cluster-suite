b322ffe4fefdf3ef53d3c9dc900902bdc08a3182
marc <marc>
2007-03-26 08:30:27 +0000

    - cleaned up a little bit - fixed a bug that only occured with methods with journal-methods with arguments

--- lib/comoonics/ComJournaled.py
+++ lib/comoonics/ComJournaled.py
@@ -6,11 +6,11 @@ here should be some more information about the module, that finds its way inot t
 """
 
 # here is some internal information
-# $Id: ComJournaled.py,v 1.1 2006-07-19 14:29:15 marc Exp $
+# $Id: ComJournaled.py,v 1.2 2007-03-26 08:30:27 marc Exp $
 #
 
 
-__version__ = "$Revision: 1.1 $"
+__version__ = "$Revision: 1.2 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/Attic/ComJournaled.py,v $
 
 import ComLog
@@ -22,7 +22,7 @@ class JournaledObject:
     If undo is called the journal stack is executed from top to buttom (LIFO) order.
     """
     __logStrLevel__ = "JournaledObject"
-    
+
     class JournalEntry:
         """
         An entry in the journal
@@ -36,29 +36,29 @@ class JournaledObject:
                 self.params=params[2:]
             else:
                 self.params=None
-    
+
     def __init__(self):
         self.__journal__=list()
         self.__undomap__=dict()
-        
+
     def addToUndoMap(self, classname, methodname, undomethod):
         """
-        Adds a new entry to the undomap. We add a 3 tuple to every undomap. Key is the methodname and undomethod and 
+        Adds a new entry to the undomap. We add a 3 tuple to every undomap. Key is the methodname and undomethod and
         refobject are the values to be executed on replayJournal.
         """
         self.__undomap__[classname+"."+methodname]=undomethod
-        
+
     def removeFromUndoMap(self, classname, methodname):
         """
         Removes a already added method from the undomap
         """
         if self.__undomap__.hasKey(classname+"."+methodname):
             self.__undomap__.pop(classname+"."+methodname)
-            
+
     def journal(self, *params):
         """
         Adds an entry to the journal list
-        
+
         params - must be a list of at least 2 params (ref and methodname). Optionally the rest are parameters to the
                  method
         """
@@ -68,7 +68,7 @@ class JournaledObject:
             self.__journal__.append(JournaledObject.JournalEntry(params[0], params[1], params[2:]))
         else:
             self.__journal__.append(JournaledObject.JournalEntry(params[0], params[1]))
-        
+
     def replayJournal(self):
         """
         replays the journal from top to buttom. The last inserted method first.
@@ -78,23 +78,24 @@ class JournaledObject:
             ComLog.getLogger(JournaledObject.__logStrLevel__).debug("Journalentry: %s, %s" %(je.method, je.ref.__class__))
             undomethod=""
             try:
-                undomethod=self.__undomap__[je.ref.__class__.__name__+"."+je.method]
+                undomethod=self.__undomap__[str(je.ref.__class__.__name__+"."+je.method)]
                 ComLog.getLogger(JournaledObject.__logStrLevel__).debug("Undomethod: %s(%s)" % (undomethod, je.params))
-                if not je.params or len(je.params) == 0:
-                    # changed call of replay 
+                if not je.params or (type(je.params)==list and len(je.params) == 0):
+                    # changed call of replay
                     # __class__.__dict__[undomethod] does not work with inherited methods
                     #je.ref.__class__.__dict__[undomethod](je.ref)
                     getattr(je.ref,undomethod)()
+                elif type(je.params)!=list and type(je.params)!=tuple:
+                    getattr(je.ref, undomethod)(je.params)
                 elif len(je.params) == 1:
                     #je.ref.__class__.__dict__[undomethod](je.ref, je.params[0])
                     getattr(je.ref,undomethod)(je.params[0])
                 else:
                     #je.ref.__class__.__dict__[undomethod](je.ref, je.params)
-                    getattr(je.ref,undomethod)(je.params)
+                    getattr(je.ref,undomethod)(*je.params)
             except Exception, e:
                 ComLog.getLogger(JournaledObject.__logStrLevel__).warn("Caught exception e during journal replay of method %s => %s on %s: %s" % (je.method, undomethod, je.ref.__class__.__name__, e))
-                import traceback
-                traceback.print_exc()
+                ComLog.debugTraceLog(ComLog.getLogger(JournaledObject.__logStrLevel__))
     def commitJournal(self):
         """
         commits the journal, i.e. deletes all journal entries
@@ -103,7 +104,11 @@ class JournaledObject:
 
 ####################
 # $Log: ComJournaled.py,v $
-# Revision 1.1  2006-07-19 14:29:15  marc
+# Revision 1.2  2007-03-26 08:30:27  marc
+# - cleaned up a little bit
+# - fixed a bug that only occured with methods with journal-methods with arguments
+#
+# Revision 1.1  2006/07/19 14:29:15  marc
 # removed the filehierarchie
 #
 # Revision 1.5  2006/07/06 11:52:19  mark
