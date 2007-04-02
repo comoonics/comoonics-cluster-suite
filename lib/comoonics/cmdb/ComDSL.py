074f370004347c487dd83f6d7e5cf79a4565d32a
marc <marc>
2007-04-02 11:16:41 +0000

    For Hilti RPM Control: - added DBLogging

--- lib/comoonics/cmdb/ComDSL.py
+++ lib/comoonics/cmdb/ComDSL.py
@@ -4,12 +4,13 @@ Class for the dsl
 Methods for filling and getting informations from the definte software library (DSL)
 """
 # here is some internal information
-# $Id: ComDSL.py,v 1.1 2007-02-23 12:42:23 marc Exp $
+# $Id: ComDSL.py,v 1.2 2007-04-02 11:16:41 marc Exp $
 #
 
 import os
 from comoonics.cmdb.ComBaseDB import BaseDB
 from comoonics import ComLog
+from comoonics.db.ComDBLogger import DBLogger
 
 class DSL(BaseDB):
     """
@@ -41,15 +42,23 @@ class DSL(BaseDB):
         else:
             return ("unknownchannel", "noversion")
 
-    def updateRPM(self, rpm, rpmfile, channel, channelversion, channeldir):
-        return super(DSL, self).updateRPM("INSERT INTO %s VALUES(\"rpm\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s/%s\");" % (self.tablename, channel, channelversion, rpm["name"], rpm["release"], rpm["version"], rpm["arch"], channeldir, rpmfile),
-                      "UPDATE %s SET channel=\"%s\", channelversion=\"%s\", name=\"%s\", version=\"%s\", subversion=\"%s\", architecture=\"%s\", file=\"%s/%s\" WHERE name=\"%s\";" % (self.tablename, channel, channelversion, rpm["name"], rpm["release"], rpm["version"], rpm["arch"], channeldir, rpmfile, rpm["name"]),
-                      "SELECT name, version, subversion AS \"release\", architecture AS \"arch\" FROM %s WHERE name=\"%s\" AND version=\"%s\" AND subversion=\"%s\" AND architecture=\"%s\";" %(self.tablename, rpm["name"], rpm["release"], rpm["version"], rpm["arch"]),
-                      rpm)
+    def updateRPM(self, _rpm, rpmfile, channel, channelversion, channeldir):
+        ret=super(DSL, self).updateRPM("INSERT INTO %s VALUES(\"rpm\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s/%s\");" % (self.tablename, channel, channelversion, _rpm["name"], _rpm["release"], _rpm["version"], _rpm["arch"], channeldir, rpmfile),
+                  "UPDATE %s SET channel=\"%s\", channelversion=\"%s\", name=\"%s\", version=\"%s\", subversion=\"%s\", architecture=\"%s\", file=\"%s/%s\" WHERE name=\"%s\";" % (self.tablename, channel, channelversion, _rpm["name"], _rpm["release"], _rpm["version"], _rpm["arch"], channeldir, rpmfile, _rpm["name"]),
+                  "SELECT name, version, subversion AS \"release\", architecture AS \"arch\" FROM %s WHERE name=\"%s\" AND version=\"%s\" AND subversion=\"%s\" AND architecture=\"%s\";" %(self.tablename, _rpm["name"], _rpm["release"], _rpm["version"], _rpm["arch"]),
+                   _rpm)
+        if ret==1:
+            self.dblog.log(DBLogger.DB_LOG_LEVEL, "Added new software package %s-%s.%s.%s to channel %s, %s (table: %s)" %(_rpm["name"], _rpm["version"], _rpm["release"], _rpm["arch"], channel, channelversion, self.tablename))
+        elif ret>1:
+            self.dblog.log(DBLogger.DB_LOG_LEVEL, "Updated existing software package %s-%s.%s.%s in channel %s, %s (table: %s)" %(_rpm["name"], _rpm["version"], _rpm["release"], _rpm["arch"], channel, channelversion, self.tablename))
 
 
 ######################
 # $Log: ComDSL.py,v $
-# Revision 1.1  2007-02-23 12:42:23  marc
+# Revision 1.2  2007-04-02 11:16:41  marc
+# For Hilti RPM Control:
+# - added DBLogging
+#
+# Revision 1.1  2007/02/23 12:42:23  marc
 # initial revision
 #
