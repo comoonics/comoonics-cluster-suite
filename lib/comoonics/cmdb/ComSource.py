cfc1d8970fe1ec087d1158cfa7e0130f1cdc8762
marc <marc>
2007-04-02 11:16:00 +0000

    For Hilti RPM Control: - added DBLogging - changed getSourceForCategory to getSourcesForCategory

--- lib/comoonics/cmdb/ComSource.py
+++ lib/comoonics/cmdb/ComSource.py
@@ -4,13 +4,14 @@ Class for the dsl
 Methods for filling and getting informations for sources for CMDB
 """
 # here is some internal information
-# $Id: ComSource.py,v 1.2 2007-03-06 06:54:15 marc Exp $
+# $Id: ComSource.py,v 1.3 2007-04-02 11:16:00 marc Exp $
 #
 
 import os
 from comoonics.cmdb.ComBaseDB import BaseDB
 from comoonics import ComLog
 from comoonics.ComSystemInformation import RedhatClusterSystemInformation
+from comoonics.db.ComDBLogger import DBLogger
 
 class Source(BaseDB):
     """
@@ -69,19 +70,19 @@ class Source(BaseDB):
             row=rs.fetch_row()
         return categories
 
-    def getSourceForCategory(self, category):
+    def getSourcesForCategory(self, category):
         """
         Returns the category for the given sourcename
         """
-        selectquery="SELECT DISTINCT name FROM %s WHERE category=\"%s\" LIMIT 1" %(self.tablename, category)
-        self.log.debug("select %s" % selectquery)
+        selectquery="SELECT DISTINCT name FROM %s WHERE category=\"%s\";" %(self.tablename, category)
         rs=self.selectQuery(selectquery)
-        categories=list()
+        sources=list()
         row=rs.fetch_row()
-        if len(row)==0:
-            return None
-        else:
-            return row[0][0]
+        while row:
+            sources.append(row[0][0])
+            row=rs.fetch_row()
+        self.log.debug("select %s %u, %u" %(selectquery, rs.num_rows(), len(sources)))
+        return sources
 
     def updateSystemInformation(self, sysinfo, category):
         values=dict()
@@ -95,14 +96,22 @@ class Source(BaseDB):
 #        values["architecture"]=sysinfo.getArchitecture()
 #        values["operating_system"]=sysinfo.getOperatingsystem()
 #        values["kernel_version"]=sysinfo.getKernelversion()
-        return super(Source, self).updateRPM("INSERT INTO %s VALUES(\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", NOW());" % (self.tablename, source_type, sysinfo.getName(), category, sysinfo.getArchitecture(), sysinfo.getOperatingsystem(), sysinfo.getKernelversion(), sysinfo.getUptime()),
-                      "UPDATE %s SET source_type=\"%s\", category=\"%s\", architecture=\"%s\", operating_system=\"%s\", kernel_version=\"%s\", uptime=\"%s\", lastimport=Now() WHERE name=\"%s\";" % (self.tablename, source_type, category, sysinfo.getArchitecture(), sysinfo.getOperatingsystem(), sysinfo.getKernelversion(), sysinfo.getUptime(), sysinfo.getName()),
-                      "SELECT name, source_type, category, architecture, operating_system, kernel_version, uptime FROM %s WHERE name=\"%s\";" %(self.tablename, sysinfo.getName()), values, values.keys())
-
+        ret=super(Source, self).updateRPM("INSERT INTO %s VALUES(\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", NOW());" % (self.tablename, source_type, sysinfo.getName(), category, sysinfo.getArchitecture(), sysinfo.getOperatingsystem(), sysinfo.getKernelversion(), sysinfo.getUptime()),
+                  "UPDATE %s SET source_type=\"%s\", category=\"%s\", architecture=\"%s\", operating_system=\"%s\", kernel_version=\"%s\", uptime=\"%s\", lastimport=Now() WHERE name=\"%s\";" % (self.tablename, source_type, category, sysinfo.getArchitecture(), sysinfo.getOperatingsystem(), sysinfo.getKernelversion(), sysinfo.getUptime(), sysinfo.getName()),
+                  "SELECT name, source_type, category, architecture, operating_system, kernel_version, uptime FROM %s WHERE name=\"%s\";" %(self.tablename, sysinfo.getName()), None)
+        if ret==1:
+            self.dblog.log(DBLogger.DB_LOG_LEVEL, "Added new system %s, %s (table: %s)" %(sysinfo.getName(), category, self.tablename))
+        elif ret>1:
+            self.dblog.log(DBLogger.DB_LOG_LEVEL, "Updated existing system %s, %s (table: %s)" %(sysinfo.getName(), category, self.tablename))
 
 ######################
 # $Log: ComSource.py,v $
-# Revision 1.2  2007-03-06 06:54:15  marc
+# Revision 1.3  2007-04-02 11:16:00  marc
+# For Hilti RPM Control:
+# - added DBLogging
+# - changed getSourceForCategory to getSourcesForCategory
+#
+# Revision 1.2  2007/03/06 06:54:15  marc
 # be compatible with mysql4.1 and mysql-python on rhel4
 #
 # Revision 1.1  2007/03/05 16:10:30  marc
