8107c513ed95aee3901381deefe8dba14fff7a56
marc <marc>
2007-04-02 11:12:37 +0000

    For Hilti RPM Control: - added BaseClass DBConnection - changed updateRPM when no select parameters were give to update then

--- lib/comoonics/cmdb/ComBaseDB.py
+++ lib/comoonics/cmdb/ComBaseDB.py
@@ -4,14 +4,17 @@ Class for the BaseDB
 FIXME: Should become a singleton based on table, dbname, user, pw
 """
 # here is some internal information
-# $Id: ComBaseDB.py,v 1.4 2007-03-14 16:51:21 marc Exp $
+# $Id: ComBaseDB.py,v 1.5 2007-04-02 11:12:37 marc Exp $
 #
 
 import MySQLdb
+import logging
 
 from comoonics import ComLog
+from comoonics.db.ComDBLogger import DBLogger
+from comoonics.db.ComDBConnection import DBConnection
 
-class BaseDB(object):
+class BaseDB(DBConnection):
     """
     Class for the cmdb representing a database connection an a tablename
     """
@@ -39,7 +42,7 @@ class BaseDB(object):
         if type(orderby)==list:
             orderby.reverse()
             for ob in orderby:
-                orderbyclause+=", "+BaseDB.formatToSQLCompat(ob)
+                orderbyclause+=", \""+ob+"\""
             orderbyclause=orderbyclause[2:]
         if type(orderby)==str:
             orderbyclause=orderby
@@ -73,47 +76,72 @@ class BaseDB(object):
         __init__(hostname=.., user=.., password=.., database=.., tablename=..)
         __init__(dbhandle=.., tablename=..)
         """
-        if kwds.has_key("dbhandle"):
-            self.db=kwds["dbhandle"]
-        else:
-            self.db=MySQLdb.connect(host=kwds["hostname"], user=kwds["user"], passwd=kwds["password"], db=kwds["database"])
+        super(BaseDB, self).__init__(**kwds)
         self.tablename=kwds["tablename"]
+        self.dblog=logging.getLogger("DB::"+self.__class__.__name__)
+        if hasattr(self, "logsource"):
+            self.dblog.addHandler(DBLogger(dbhandle=self.db, logsource=getattr(self, "logsource")))
+        else:
+            self.dblog.addHandler(DBLogger(dbhandle=self.db))
+        self.dblog.setLevel(DBLogger.DB_LOG_LEVEL)
 
-    def selectQuery(self, query):
-        """ Returns the recordset for this query. Where query is a SQL Query directly executen on the
-            databasehandle """
-        self.db.query(query)
-        rs=self.db.store_result()
-        return rs
+    def escapeSQL(self, toescape):
+        ret=None
+        if isinstance(toescape, basestring):
+            ret=self.db.escape_string(toescape)
+        elif type(toescape)==list:
+            ret=list()
+            for name in toescape:
+                ret.append(self.escapeSQL(name))
+        else:
+            ret=self.db.escape("%s" %(toescape))
+        return ret
 
-    def updateRPM(self, insertquery, updatequery, selectquery, rpm, keys=["name", "version", "release", "arch"], keymapping=None):
+#    def selectQuery(self, query):
+#        """ Returns the recordset for this query. Where query is a SQL Query directly executen on the
+#            databasehandle """
+#        self.db.query(query)
+#        rs=self.db.store_result()
+#        return rs
+
+    def updateRPM(self, insertquery, updatequery, selectquery, _rpm, keys=["name", "version", "release", "arch"], keymapping=None):
         """
         Executes the updatequery selectquery returns more then 0 rows and the rpm unsuccessfully compared.
         rpm: if not given updatequery is always applyed if selectquery returns more then 0 rows
         keys: keys to compare with default (name, version, release, arch) selectquery has to be formulated apropriate
+        returns: 0 if nothing was updated, 1 if inserted and 2 if updated
         """
         #self.log.debug("selectquery: %s" % selectquery)
+        _return=0
         rs=self.selectQuery(selectquery)
         rows=rs.fetch_row(1, 2)
+        log_array=list()
+
         if len(rows) == 0:
-            self.log.debug("insert %s" % insertquery)
+            #self.log.debug("insert %s" % insertquery)
             self.db.query(insertquery)
+            _return=1
         else:
             ret=False
             row=rows[0]
             prefix=self.tablename+"."
-            for key in keys:
+            if keys and _rpm:
+                for key in keys:
 #                self.log.debug("mappingkey: "+key+", keymapping %s" %(keymapping))
-                if keymapping and type(keymapping) == dict and keymapping.has_key(key) and keymapping[key] != None:
-                    mapping=keymapping[key]
-                else:
-                    mapping=rpm[key]
+                    if keymapping and type(keymapping) == dict and keymapping.has_key(key) and keymapping[key] != None:
+                        mapping=keymapping[key]
+                    else:
+                        mapping=_rpm[key]
 
-                ret=ret or row[prefix+key] != mapping
+                    ret=ret or row[prefix+key] != mapping
 #                self.log.debug("row[%s]:%s == %s: ret: %s" %(prefix+key, row[prefix+key], mapping, ret))
+            else:
+                ret=True
             if ret:
-                self.log.debug("update %s" % updatequery)
+                #self.log.debug("update %s" % updatequery)
                 self.db.query(updatequery)
+                _return=2
+        return _return
 
 def test():
     testlist=[ "a", "b", "c", "d" ]
@@ -125,7 +153,12 @@ if __name__=="__main__":
 
 ########################
 # $Log: ComBaseDB.py,v $
-# Revision 1.4  2007-03-14 16:51:21  marc
+# Revision 1.5  2007-04-02 11:12:37  marc
+# For Hilti RPM Control:
+# - added BaseClass DBConnection
+# - changed updateRPM when no select parameters were give to update then
+#
+# Revision 1.4  2007/03/14 16:51:21  marc
 # fixed update error
 #
 # Revision 1.3  2007/03/14 13:16:48  marc
