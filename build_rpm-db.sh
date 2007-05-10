16d88bbd3471ccfa6b95288618b98972d5a4826e
marc <marc>
2007-05-10 08:24:32 +0000

    MMG Support
    Hilti RPM Control

--- build_rpm-db.sh
+++ build_rpm-db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# $Id: build_rpm-db.sh,v 1.3 2007-04-18 10:25:35 marc Exp $
+# $Id: build_rpm-db.sh,v 1.4 2007-05-10 08:24:32 marc Exp $
 
 source ./build-lib.sh
 
-RELEASE=6
+RELEASE=7
 REQUIRES="--requires=comoonics-cs-py,MySQL-python"
 NOAUTO_REQ="--no-autoreq"
 NAME="comoonics-db-py"
@@ -24,7 +24,12 @@ setup
 
 ##############
 # $Log: build_rpm-db.sh,v $
-# Revision 1.3  2007-04-18 10:25:35  marc
+# Revision 1.4  2007-05-10 08:24:32  marc
+# MMG Support
+#
+# Hilti RPM Control
+#
+# Revision 1.3  2007/04/18 10:25:35  marc
 # Hilti RPM Control
 # - version cmdb-21 and db-6
 #
