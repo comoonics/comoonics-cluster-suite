a397b31474169761e37e00acebba76995a6824a8
marc <marc>
2007-06-13 09:17:22 +0000

    - better testing, but obsolete??

--- test/testlog.py
+++ test/testlog.py
@@ -1,3 +1,4 @@
+# $Id: testlog.py,v 1.3 2007-06-13 09:17:22 marc Exp $
 import sys
 import traceback
 
@@ -5,15 +6,25 @@ sys.path.append("../lib")
 #print sys.path
 
 try:
-    from comoonics import ComLog 
+    from comoonics import ComLog
     from comoonics import ComSystem
 except ImportError:
-    print "Exception in import " 
+    print "Exception in import "
     print traceback.print_exc()
 
 log=ComLog.getLogger()
+print log
 log.critical("This is really critical");
 
-ComSystem.execLocal("echo \"hallo du\"")
+try:
+    raise TypeError, "bogus typerror for testing"
+except Exception, e:
+    log.exception("Huston, we have a problem")
 
+#ComSystem.execLocal("echo \"hallo du\"")
 
+#################
+# $Log: testlog.py,v $
+# Revision 1.3  2007-06-13 09:17:22  marc
+# - better testing, but obsolete??
+#
