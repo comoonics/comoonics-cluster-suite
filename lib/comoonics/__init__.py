c20fda9e51d1d115604104cbba47b3957a84c199
marc <marc>
2007-06-13 09:15:12 +0000

    - added asConfigParser

--- lib/comoonics/__init__.py
+++ lib/comoonics/__init__.py
@@ -12,9 +12,17 @@ except ImportError:
 #    print "Importerror of comoonics.backup"
             pass
 
+def asConfigParser():
+    from comoonics import XMLConfigParser
+    import sys
+    sys.modules["ConfigParser"]=XMLConfigParser
+
 ########
 # $Log: __init__.py,v $
-# Revision 1.2  2007-03-26 08:15:21  marc
+# Revision 1.3  2007-06-13 09:15:12  marc
+# - added asConfigParser
+#
+# Revision 1.2  2007/03/26 08:15:21  marc
 # automatically importing comoonics.backup if available
 #
 # Revision 1.1  2006/07/19 14:29:15  marc
