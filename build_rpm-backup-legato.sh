fdf165ec1b71d8ee6646c020f583dfb04d70f08e
marc <marc>
2007-07-10 11:38:25 +0000

    new version 3

--- build_rpm-backup-legato.sh
+++ build_rpm-backup-legato.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# $Id: build_rpm-backup-legato.sh,v 1.2 2007-06-13 09:02:08 marc Exp $
+# $Id: build_rpm-backup-legato.sh,v 1.3 2007-07-10 11:38:25 marc Exp $
 
 source ./build-lib.sh
 
-RELEASE=2
+RELEASE=3
 REQUIRES="--requires=comoonics-cs-py,comoonics-backup-py"
 NOAUTO_REQ="--no-autoreq"
 NAME="comoonics-backup-legato-py"
@@ -21,7 +21,10 @@ setup
 
 ##############
 # $Log: build_rpm-backup-legato.sh,v $
-# Revision 1.2  2007-06-13 09:02:08  marc
+# Revision 1.3  2007-07-10 11:38:25  marc
+# new version 3
+#
+# Revision 1.2  2007/06/13 09:02:08  marc
 # - now backuping full path to support incremental backups (0.1-2)
 #
 # Revision 1.1  2007/04/04 13:42:42  marc
