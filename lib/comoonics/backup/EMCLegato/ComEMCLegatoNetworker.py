e5907e25e5509e2f7a24fbfb03d9fad4f6e001ec
marc <marc>
2007-07-10 11:33:24 +0000

    changed way to restore as all files would be restored in the basedir of all files which is not what we want.

--- lib/comoonics/backup/EMCLegato/ComEMCLegatoNetworker.py
+++ lib/comoonics/backup/EMCLegato/ComEMCLegatoNetworker.py
@@ -2,11 +2,12 @@
 Class for the EMC-Legator BackupHandlerImplementation.
 """
 # here is some internal information
-# $Id: ComEMCLegatoNetworker.py,v 1.2 2007-04-04 12:47:08 marc Exp $
+# $Id: ComEMCLegatoNetworker.py,v 1.3 2007-07-10 11:33:24 marc Exp $
 #
 
-
-from comoonics import ComLog, ComSystem
+import os.path
+from logging import DEBUG
+from comoonics import ComLog, ComSystem, pexpect
 from comoonics.ComSystem import execLocalOutput
 from comoonics.ComExceptions import ComException
 
@@ -36,11 +37,13 @@ class LegatoNetworkerRecoverError(LegatoNetworkerError):
         return "Error during the execution of %s. Error: %s" %(LegatoNetworker.LEGATO_CMD_RECOVER, self.value)
 
 class LegatoNetworker(object):
-    log=ComLog.getLogger("LegatoNetworker")
+    log=ComLog.getLogger("comoonics.backup.EMCLegato.LegatoNetworker")
     LEGATO_CMD_SAVEFS="/usr/sbin/savefs"
     LEGATO_CMD_RECOVER="/usr/sbin/recover"
+    LEGATO_RECOVER_SHELL="recover>"
     OPTION_NOT_IN_SAVESET="-F"
     OPTION_AUTOMATIC_RECOVERY="-a"
+    OPTION_FORCEOVERWRITE_RECOVERY="-f"
     """
     Creates a new LegatoNetworker instance for the given client and backupset
     @client: the client to backup or restore
@@ -54,26 +57,47 @@ class LegatoNetworker(object):
         self.server=server
         self.savefs_options =list()
         self.recover_options=list()
-        self.recover_options.append(self.OPTION_AUTOMATIC_RECOVERY)
+        #self.recover_options.append(self.OPTION_AUTOMATIC_RECOVERY)
+        self.recover_options.append(self.OPTION_FORCEOVERWRITE_RECOVERY)
 
     def executeSaveFs(self, level, file=None):
         values=list()
         if file:
             self.savefs_options.append(LegatoNetworker.OPTION_NOT_IN_SAVESET)
             values.append(file)
-        cmd="%s %s -l %s -c %s -s %s -g %s %s" %(LegatoNetworker.LEGATO_CMD_SAVEFS, " ".join(self.savefs_options), level, \
+        _cmd="%s %s -l %s -c %s -s %s -g %s %s" %(LegatoNetworker.LEGATO_CMD_SAVEFS, " ".join(self.savefs_options), level, \
                                                  self.client, self.server, self.group, " ".join(values))
-        ComSystem.execLocalOutput(cmd)
+        _output=ComSystem.execLocalOutput(_cmd)
+        self.log.debug("executeSaveFs: cmd=%s, output=%s" %(_cmd, _output))
 
-    def executeRecover(self, file, destdir):
+    def executeRecover(self, _file, destdir, dir=True):
         # recover -s wspbackup.messe-muenchen.de -c wspbackup.messe-muenchen.de -a -d /mnt/backup/recover /tmp/metadata/vg_gfstest-lv_gfstest_filesystem.xml
-        cmd="%s %s -c %s -s %s -d %s %s" %(LegatoNetworker.LEGATO_CMD_RECOVER, " ".join(self.recover_options),
-                                              self.client, self.server, destdir, file)
-        ComSystem.execLocalOutput(cmd)
+        if dir:
+            _dir=_file
+            _selection="*"
+        else:
+            _dir=os.path.dirname(_file)
+            _selection=os.path.basename(_file)
+        _cmd="%s %s -c %s -s %s -d %s %s" %(LegatoNetworker.LEGATO_CMD_RECOVER, " ".join(self.recover_options),
+                                            self.client, self.server, destdir, _dir)
+        if ComSystem.askExecModeCmd(_cmd):
+            _shell=pexpect.spawn(_cmd)
+            if self.log.getEffectiveLevel() == DEBUG:
+                _shell.logfile=file(str("/tmp/%s.log" %(os.path.basename(LegatoNetworker.LEGATO_CMD_RECOVER))), "w")
+                _shell.cmdlogfile=file(str("/tmp/%s-cmd.log" %(os.path.basename(LegatoNetworker.LEGATO_CMD_RECOVER))), "w")
+            _shell.expect(self.LEGATO_RECOVER_SHELL)
+            _shell.sendline("add -q %s" %_selection)
+            _shell.expect(self.LEGATO_RECOVER_SHELL)
+            _shell.sendline("recover")
+            _shell.expect(self.LEGATO_RECOVER_SHELL, None)
+            _shell.close()
 
 #######################
 # $Log: ComEMCLegatoNetworker.py,v $
-# Revision 1.2  2007-04-04 12:47:08  marc
+# Revision 1.3  2007-07-10 11:33:24  marc
+# changed way to restore as all files would be restored in the basedir of all files which is not what we want.
+#
+# Revision 1.2  2007/04/04 12:47:08  marc
 # MMG Backup Legato Integration:
 # - added executeRecover for restoring data
 #
