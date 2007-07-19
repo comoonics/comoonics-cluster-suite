43b8bb1c60b6a604e13df7a3c1a1f3704c6392ab
marc <marc>
2007-07-19 12:04:39 +0000

    new version 0.1-40 (com-gfs_tool)

--- build_rpm-cs.sh
+++ build_rpm-cs.sh
@@ -4,7 +4,7 @@
 
 source ./build-lib.sh
 
-RELEASE=39
+RELEASE=40
 REQUIRES="--requires=PyXML,pyparted"
 NOAUTO_REQ="--no-autoreq"
 NAME="comoonics-cs-py"
@@ -18,7 +18,7 @@ AUTHOR_EMAIL="grimme@atix.de"
 URL="http://www.atix.de/comoonics/"
 PACKAGE_DIR='"comoonics" : "lib/comoonics"'
 PACKAGES='"comoonics"'
-SCRIPTS='"bin/com-dsh", "bin/cl_checknodes"'
+SCRIPTS='"bin/com-dsh", "bin/cl_checknodes", "bin/com-gfs_tool"'
 
 setup
 
