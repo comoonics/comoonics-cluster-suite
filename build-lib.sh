c583c17a40feaf2a2e07b13acf0b5db39713027c
andrea2 <andrea2>
2007-08-22 12:35:44 +0000

    *** empty log message ***

--- build-lib.sh
+++ build-lib.sh
@@ -1,4 +1,4 @@
-# $Id: build-lib.sh,v 1.4 2007-08-21 13:19:02 andrea2 Exp $
+# $Id: build-lib.sh,v 1.5 2007-08-22 12:40:13 andrea2 Exp $
 
 function setup {
   CHANGELOG=$(awk '
@@ -15,6 +15,35 @@ BEGIN { changelogfound=0; }
   [ -e MANIFEST ] && rm MANIFEST
 
 [ -e setup.py ] && rm -f setup.py
+
+echo '#!/usr/bin/python
+from comoonics import ComSystem
+import re
+import os
+import gzip
+
+ComSystem.__EXEC_REALLY_DO="continue"
+manpages = "'${NAME}'.xml"
+olddir=os.path.abspath(os.path.curdir)
+os.chdir("man/")
+if os.path.exists(manpages):
+	ComSystem.execLocalStatusOutput("db2x_docbook2man " + manpages)
+	man = '${DATA_FILES}'[1]
+	for i in man:
+		inF = file("../"+i.replace(".gz","",1),"rb")
+		s = inF.read()
+		inF.close()
+		
+		outF = gzip.GzipFile("../"+i,"wb")
+		outF.write(s)
+		outF.close()
+
+		os.remove("../"+i.replace(".gz","",1))
+		
+os.chdir(olddir)
+
+' > doc2man.py
+  python doc2man.py
 
 echo '#!/usr/bin/python
 from distutils.core import setup
@@ -36,8 +65,8 @@ setup(name="'${NAME}'",
 }
 ##########
 # $Log: build-lib.sh,v $
-# Revision 1.4  2007-08-21 13:19:02  andrea2
-# added support for extra files
+# Revision 1.5  2007-08-22 12:40:13  andrea2
+# *** empty log message ***
 #
 # Revision 1.3  2007/04/02 12:09:38  marc
 # stable changelog generation
