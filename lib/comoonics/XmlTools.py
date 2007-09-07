8e9cd693bd28845bebf4537ca995f830264031e6
marc <marc>
2007-09-07 14:49:07 +0000

    - logging - better testing - added clone_node - extended merge_tree

--- lib/comoonics/XmlTools.py
+++ lib/comoonics/XmlTools.py
@@ -5,10 +5,13 @@ Collection of xml tools
 
 __version__= "$Revision $"
 
-# $Id: XmlTools.py,v 1.5 2007-03-12 17:03:18 mark Exp $
+# $Id: XmlTools.py,v 1.6 2007-09-07 14:49:07 marc Exp $
 
 import warnings
 from xml.dom import Node
+from comoonics import ComLog
+
+logger=ComLog.getLogger("comoonics.XmlTools")
 
 class ElementFilter(object):
     FILTER_ACCEPT = 1
@@ -38,11 +41,12 @@ def overwrite_element_with_xpaths(element, xpaths):
     for xpath in xpaths.keys():
         try:
             import xml
-
+            logger.debug("overwrite_element_with_xpaths xpath %s=%s, rootnode: %s, type(element): %s, class(element): %s" %(xpath, xpaths[xpath], element, type(element), element.__class__))
             if isinstance(element, xml.dom.Node):
                 from xml.dom   import Element, Node
                 from xml.xpath import Evaluate
                 sets = Evaluate(xpath, element)
+                logger.debug("overwrite_element_with_xpaths found %u matches. overwriting." %len(sets))
                 for set in sets:
                     set.nodeValue=xpaths[xpath]
             else:
@@ -50,9 +54,9 @@ def overwrite_element_with_xpaths(element, xpaths):
                 ctxt = element.xpathNewContext()
                 sets = ctxt.xpathEvalExpression(xpath)
                 for set in sets:
-                    print "%s, %s" %(set.name, set.content)
+                    logger.debug("%s, %s" %(set.name, set.content))
                     set.setContent(xpaths[xpath])
-                    print "%s, %s" %(set.name, set.content)
+                    logger.debug("%s, %s" %(set.name, set.content))
 #                import StringIO
 #                ctxt.xpathFreeContext()
 #                f = StringIO.StringIO()
@@ -66,7 +70,7 @@ def overwrite_element_with_xpaths(element, xpaths):
 
     return element
 
-def merge_trees_with_pk(source, dest, doc, pk="name", filter=None):
+def merge_trees_with_pk(source, dest, doc, pk="name", filter=None, onlyone=False, takesource=False):
     """ add all element children from element source to
     if they are not already there.
     doc is the destination DOMDocument
@@ -74,6 +78,7 @@ def merge_trees_with_pk(source, dest, doc, pk="name", filter=None):
     If filter [NodeFilter] is the DOM2 Nodefilter interface is applied to any element to be checked.
     True and False are to be returned
     Also adds all Attributes from dataobject if the are not present.
+    If onlyone then only one child with the same pk is taken.
     """
     #get source childs
 
@@ -95,7 +100,14 @@ def merge_trees_with_pk(source, dest, doc, pk="name", filter=None):
         # do we already have this child ?
         #elems=self.element.getElementsByTagName(tagname)
         # no we don't
-        if not xpath.Evaluate(tagname+"/@"+pk+"='"+pkval+"'", dest):
+
+        #logger.debug("merge_trees_with_pk xpath: %s/@%s='%s'" %(tagname, pk, pkval))
+        try:
+            _path=xpath.Evaluate(tagname+"/@"+pk+"='"+pkval+"'", dest)
+        except xpath.pyxpath.SyntaxError:
+            #ComLog.debugTraceLog(logger)
+            _path=False
+        if not _path:
             #print "we don't have this element, adding"
             # lets copy things from the source
             # - create new element
@@ -105,7 +117,8 @@ def merge_trees_with_pk(source, dest, doc, pk="name", filter=None):
                 d_child.setAttribute(attrnode.name, s_child.getAttribute(attrnode.name))
             # - add child
             #print "add new child"
-            add_element_to_node_sorted(d_child, dest, pk)
+            if not onlyone:
+                add_element_to_node_sorted(d_child, dest, pk)
 
         # yes we have
         else:
@@ -118,8 +131,49 @@ def merge_trees_with_pk(source, dest, doc, pk="name", filter=None):
             if not d_child.hasAttribute(attrnode.name):
                 d_child.setAttribute(attrnode.name, s_child.getAttribute(attrnode.name))
         # recursion on child
-        merge_trees_with_pk(s_child, d_child, doc, pk)
+        merge_trees_with_pk(s_child, d_child, doc, pk, filter, onlyone)
 
+def clone_node(node, doc=None):
+    """
+    clones the given node by creating a new one
+    """
+    import xml.dom
+    if not doc:
+        _impl=xml.dom.getDOMImplementation()
+        doc=_impl.createDocument(None, node.tagName, None)
+    if node.nodeType==xml.dom.Node.ELEMENT_NODE:
+        newnode=doc.createElement(node.tagName)
+        for _child in node.childNodes:
+            newnode.appendChild(clone_node(_child, doc))
+        for _i in range(node.attributes.length):
+            _attr=node.attributes.item(_i)
+            newnode.setAttribute(_attr.nodeName, _attr.nodeValue)
+        return newnode
+    elif node.nodeType==xml.dom.Node.TEXT_NODE or node.nodeType==xml.dom.Node.CDATA_SECTION_NODE:
+        return doc.createTextNode(node.data)
+    elif node.nodeType==xml.dom.Node.PROCESSING_INSTRUCTION_NODE:
+        return doc.createProcessingInstruction(node.target, node.data)
+    elif node.nodeType==xml.dom.Node.COMMENT_NODE:
+        return doc.createComment(node.data)
+    else:
+        return node.cloneNode(1)
+
+def add_element_to_node(child, element, doc=None):
+    """
+    adds an element @child to the element tree @element. The child is copied.
+    """
+    import xml.dom
+    if not doc:
+        _impl=xml.dom.getDOMImplementation()
+        doc=_impl.createDocument(None, doc.documentElement.tagName, None)
+    if child.nodeType==xml.dom.Node.ELEMENT_NODE:
+        newchild=doc.createElement(child.tagName)
+        for _child in child.childNodes:
+            add_element_to_node(_child, newchild, doc)
+        element.appendChild(newchild)
+    else:
+        element.appendChild(child.cloneNode(1))
+    return element
 
 def add_element_to_node_sorted(child, elem, key):
     """ adds an  element child into the elem tree
@@ -163,7 +217,7 @@ def main():
     from xml.dom.ext import PrettyPrint
     from xml.dom.minidom import parseString
     doc=parseString(xml)
-    xpaths={"/localclone/node/@name": "myname",
+    xpaths={"node/@name": "myname",
             "/localclone/destdisks/disk/@name": "/dev/sda1"}
     print "-----------_Before(overwrite_element_with_xpaths)_-------------"
     PrettyPrint(doc)
@@ -178,12 +232,69 @@ def main():
     doc=parseString(xml3)
     print "getTextFromElement(%s): %s" %(xml3, getTextFromElement(doc.documentElement))
 
+    xml1="""
+    <a>
+       <a name="a">
+          <aa/>
+       </a>
+       <b name="a">
+          <ba/>
+       </b>
+       <c name="b">
+          <cb/>
+       </c>
+    </a>
+    """
+    xml2="""
+    <a>
+       <b name="a">
+          <bb/>
+       </b>
+       <c name="b">
+          <ca/>
+       </c>
+       <d name="a">
+         <da/>
+       </d>
+    </a>
+    """
+
+    import xml.dom
+    print "Testing merging of two documents"
+    print "xml1: "
+    _xml1=parseString(xml1)
+    PrettyPrint(_xml1)
+    print "xml2: "
+    _xml2=parseString(xml2)
+    PrettyPrint(_xml2)
+    _result=xml.dom.getDOMImplementation().createDocument(None, "a", None)
+
+    print "Result(onlyone=True): "
+    merge_trees_with_pk(_xml1.documentElement, _xml2.documentElement, _xml2, "name", None, True)
+    PrettyPrint(_xml2)
+    print "Result(onlyone=False): "
+    _xml2=parseString(xml2)
+    merge_trees_with_pk(_xml1.documentElement, _xml2.documentElement, _xml2, "name", None, False)
+    PrettyPrint(_xml2)
+
+    print "Testing clone of element: "
+    print "xml2: "
+    PrettyPrint(_xml2)
+    print "clone_node(xml2)"
+    PrettyPrint(clone_node(_xml2.documentElement))
+
 if __name__ == '__main__':
     main()
 
 #################
 # $Log: XmlTools.py,v $
-# Revision 1.5  2007-03-12 17:03:18  mark
+# Revision 1.6  2007-09-07 14:49:07  marc
+# - logging
+# - better testing
+# - added clone_node
+# - extended merge_tree
+#
+# Revision 1.5  2007/03/12 17:03:18  mark
 # Bug Fix: compares pk attributes: fixes bz #35
 #
 # Revision 1.4  2007/02/09 11:35:44  marc
