34a77c08c6c1b5dd09ec3b24e4a235e54bfc0d46
marc <marc>
2007-10-09 14:28:12 +0000

    - new version of comoonics-cs-py 0.1-47

--- build_rpm-cs.sh
+++ build_rpm-cs.sh
@@ -4,7 +4,7 @@
 
 source ./build-lib.sh
 
-RELEASE=46
+RELEASE=47
 REQUIRES="--requires=PyXML,pyparted"
 NOAUTO_REQ="--no-autoreq"
 NAME="comoonics-cs-py"
@@ -18,7 +18,7 @@ AUTHOR_EMAIL="grimme@atix.de"
 URL="http://www.atix.de/comoonics/"
 PACKAGE_DIR='"comoonics" : "lib/comoonics"'
 PACKAGES='"comoonics"'
-SCRIPTS='"bin/com-dsh", "bin/cl_checknodes", "bin/com-gfs_tool", "bin/com-sysreport" '
+SCRIPTS='"bin/com-dsh", "bin/cl_checknodes", "bin/com-gfs_tool", "bin/com-sysreport", "bin/stabilized" '
 
 setup
 
