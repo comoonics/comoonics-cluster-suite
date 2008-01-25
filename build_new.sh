f7b13ca6c9c8d59a72eb6219f82b16896d79f53e
marc <marc>
2008-01-25 10:49:52 +0000

    - new version comoonics-cs-py-0.1-49 - new version comoonics-ec-py-0.1-31

---
+++ build_new.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -f dist/*
+LASTBUILDFILE=.lastbuild
+for buildfile in $(find ./ -newer .lastbuild -type f -name "build_rpm-*.sh"); do
+   echo $buildfile
+   sh $buildfile
+done 
+#touch $LASTBUILDFILE
