65c6fc17e4b8f3a45d5bebd8a51f6ea6184f7841
marc <marc>
2008-03-12 12:28:32 +0000

    - added a constructor that also would take Paths as is - added tests. - support for simulation

--- lib/comoonics/enterprisecopy/ComPathCopyObject.py
+++ lib/comoonics/enterprisecopy/ComPathCopyObject.py
@@ -3,11 +3,11 @@ CopyObject to implement a path source/destination.
 """
 
 # here is some internal information
-# $Id: ComPathCopyObject.py,v 1.2 2008-02-29 15:02:48 mark Exp $
+# $Id: ComPathCopyObject.py,v 1.3 2008-03-12 12:28:32 marc Exp $
 #
 
 
-__version__ = "$Revision: 1.2 $"
+__version__ = "$Revision: 1.3 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/enterprisecopy/ComPathCopyObject.py,v $
 
 from ComCopyObject import CopyObjectJournaled
@@ -19,27 +19,56 @@ from comoonics.ComExceptions import ComException
 import os
 import os.path
 from xml import xpath
+import xml.dom
 
 class PathCopyObject(CopyObjectJournaled):
     __logStrLevel__="comoonics.enterprisecopy.ComPathCopyObject.PathCopyObject"
     logger=ComLog.getLogger(__logStrLevel__)
 
-    def __init__(self, element, doc):
+    def __init__(self, *params, **kwds):
+        """
+        Supported constructors
+        __init__(element, doc)
+        __init__(path, source=True|False)
+        __init__(path=path, source=True, dest=True)
+        """
         PathCopyObject.logger.debug("__init__()")
-        CopyObjectJournaled.__init__(self, element, doc)
-        try:
-            __path=xpath.Evaluate('path', element)[0]
-            self.path=Path(__path, doc)
-        except Exception:
-            raise ComException("Path for modificationset \"%s\" not defined" %self.getAttribute("name", "unknown"))
+        if (len(params)==2 and not isinstance(params[0], xml.dom.Node)) or (kwds and kwds.has_key("path") and (kwds.has_key("source") or kwds.has_key("dest"))):
+            _path=None
+            _source=(len(params)==2 and params[1]==True) or (kwds and kwds.has_key("source") and kwds["source"]==True)
+            if len(params)>=1:
+                _path=params[0]
+            else:
+                _path=kwds["path"]
+            _tagname="destination"
+            if _source:
+                _tagname="source"
+            doc=xml.dom.getDOMImplementation().createDocument(None, _tagname, None)
+            element=doc.documentElement
+            element.setAttribute("type", "path")
+            if isinstance(_path, basestring):
+                _path=Path(_path)
+            element.appendChild(_path.getElement())
+            self.path=_path
+            CopyObjectJournaled.__init__(self, element, doc)
+        else:
+            element=params[0]
+            doc=params[1]
+            CopyObjectJournaled.__init__(self, element, doc)
+            try:
+                __path=xpath.Evaluate('path', element)[0]
+                self.path=Path(__path, doc)
+            except Exception:
+                raise ComException("Path for copyobject \"%s\" not defined" %self.getAttribute("name", "unknown"))
         PathCopyObject.logger.debug("__init__(%s)" %self.path)
         self.addToUndoMap(self.path.__class__.__name__, "pushd", "popd")
 
     def __prepare(self):
         self.origpath=self.path.getPath()
-        self.path.mkdir()
-        self.path.pushd()
-        self.journal(self.path, "pushd")
+        ComSystem.execMethod(self.path.mkdir)
+        ComSystem.execMethod(self.path.pushd)
+        if not ComSystem.isSimulate():
+            self.journal(self.path, "pushd")
         PathCopyObject.logger.debug("prepareAsSource() CWD: " + os.getcwd())
 
     def __cleanup(self):
@@ -94,6 +123,10 @@ def __testPathCopyset(_modset):
     _modset.doPost()
     print "cwd: %s" %os.getcwd()
 
+def __testPathCopyObject(_copyobj):
+    from xml.dom.ext import PrettyPrint
+    PrettyPrint(_copyobj.getElement())
+
 def main():
     _xmls=[
            """
@@ -121,20 +154,33 @@ def main():
     from xml.dom.ext.reader import Sax2
     from comoonics.ComDisk import Disk
     from ComPathCopyObject import PathCopyObject
+    ComSystem.setExecMode(ComSystem.SIMULATE)
     print "registering %s" %PathCopyObject
     registerCopyObject("path", PathCopyObject)
     reader=Sax2.Reader(validate=0)
+    print "Test with xml:"
     for _xml in _xmls:
         doc=reader.fromString(_xml)
-        _modset=Copyset(doc.documentElement, doc)
-        print "Copyset: %s" %_modset
-        __testPathCopyset(_modset)
+        _copyset=Copyset(doc.documentElement, doc)
+        print "Copyset: %s" %_copyset
+        __testPathCopyset(_copyset)
+    print "Test with params"
+    _copyobj=PathCopyObject("/tmp/path", True)
+    __testPathCopyObject(_copyobj)
+    print "Test with keys"
+    _copyobj=PathCopyObject(path="/tmp/path", dest=True)
+    __testPathCopyObject(_copyobj)
 
 if __name__ == '__main__':
     main()
 
 # $Log: ComPathCopyObject.py,v $
-# Revision 1.2  2008-02-29 15:02:48  mark
+# Revision 1.3  2008-03-12 12:28:32  marc
+# - added a constructor that also would take Paths as is
+# - added tests.
+# - support for simulation
+#
+# Revision 1.2  2008/02/29 15:02:48  mark
 # workaroud for bz 204
 #
 # Revision 1.1  2007/09/07 14:39:54  marc
