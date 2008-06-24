3565b2992b99a8c245b12d8b23adeb6d25af3f2d
mark <mark>
2008-06-24 20:46:06 +0000

    new release

--- build_rpm-cluster.sh
+++ build_rpm-cluster.sh
@@ -2,7 +2,7 @@
 
 source ./build-lib.sh
 
-RELEASE=13
+RELEASE=15
 REQUIRES="--requires=comoonics-cs-py,PyXML"
 NOAUTO_REQ="--no-autoreq"
 NAME="comoonics-cluster-py"
--- build_rpm-cs.sh
+++ build_rpm-cs.sh
@@ -4,7 +4,7 @@
 
 source ./build-lib.sh
 
-RELEASE=54
+RELEASE=55
 REQUIRES="--requires=PyXML,pyparted"
 NOAUTO_REQ="--no-autoreq"
 NAME="comoonics-cs-py"
