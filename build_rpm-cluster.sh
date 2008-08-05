2840f9a82fd15983e99cd564bbbc5186fa691846
marc <marc>
2008-08-05 13:05:00 +0000

    new version: - added helper classes - added nonstatic attributes

--- build_rpm-cluster.sh
+++ build_rpm-cluster.sh
@@ -2,7 +2,7 @@
 
 source ./build-lib.sh
 
-RELEASE=15
+RELEASE=16
 REQUIRES="--requires=comoonics-cs-py,PyXML"
 NOAUTO_REQ="--no-autoreq"
 NAME="comoonics-cluster-py"
@@ -14,9 +14,9 @@ Comoonics cluster configuration utilities written in Python
 AUTHOR="ATIX AG - Andrea Offermann"
 AUTHOR_EMAIL="offermann@atix.de"
 URL="http://www.atix.de/comoonics/"
-PACKAGE_DIR='"comoonics.cluster" : "lib/comoonics/cluster"'
+PACKAGE_DIR='"comoonics.cluster" : "lib/comoonics/cluster", "comoonics.cluster.helper": "lib/comoonics/cluster/helper"'
 PACKAGE_DATA='"comoonics.cdsl": ["man/*.gz"]'
-PACKAGES='"comoonics.cluster"'
+PACKAGES='"comoonics.cluster", "comoonics.cluster.helper"'
 SCRIPTS='"bin/com-queryclusterconf"'
 DATA_FILES='("share/man/man1",[
              "man/com-queryclusterconf.1.gz"
