962ab50faaedf451a9744c0228b0fa76624943eb
mark <mark>
2008-10-22 15:58:30 +0000

    initial check in

---
+++ lib/comoonics/assistant/ComConfigurationManager.py
@@ -0,0 +1,351 @@
+"""
+The controller class for the comoonics assistant  manager
+"""
+
+import dircache
+import os
+import shutil
+
+from comoonics.ComDataObject import DataObject
+from comoonics.ComExceptions import ComException
+
+class TypeNotMatchError(ComException):
+    pass
+
+class ConfigError(ComException):
+    pass
+
+class ConfigurationManager(object):
+    
+    def __init__ (self, path="/etc/comoonics/enterprisecopy/xml-dr"):
+        self.path=os.path.normpath(path)
+        self.store=ConfigurationStore()
+
+    def setConfigPath(self, path):
+        self.path=path
+        
+    def getConfigPath(self):
+        return self.path
+    
+    def getConfigStore(self):
+        return self.store
+        
+    def getCompleteConfigSetNames(self):
+        sets={}
+        for tstore in self.store.getConfigTypeStores():
+            _all_dirs=tstore.getConfigInfodefset().getDirections()
+            for set in tstore.getConfigsets():
+                if set.hasDirections(_all_dirs):
+                    sets[set.getName()] = tstore.getType()
+        return sets
+    
+    def getCompleteConfigTemplateSetNames(self):
+        sets=[]
+        for tstore in self.store.getConfigTypeStores():
+            _all_dirs=tstore.getConfigInfodefset().getDirections()
+            if not tstore.hasConfigTemplateset(_all_dirs): continue
+            if not tstore.hasConfigInfodefset(_all_dirs): continue
+            sets.append(tstore.getType())
+        return sets
+    
+   
+    def createConfigSet(self, name, type):
+        #TODO add check for existence.
+        # check if a complete template set is available
+        if not self.store.hasConfigTypeStore(type):
+            raise ConfigError("type %s not valid" %type)
+        tstore = self.store.getConfigTypeStoreByName(type)
+        _all_dirs=tstore.getConfigInfodefset().getDirections()
+        if not tstore.hasConfigTemplateset(_all_dirs):
+            raise ConfigError("Templateset for type %s is not complete" %type)
+        tset=tstore.getConfigTemplateset()
+        for dir in _all_dirs:
+            dpath=os.path.normpath("%s/%s.%s.config.%s.xml" %(self.getConfigPath(), type, dir, name))
+            print "copy %s -> %s" %(tset.getConfiguration(dir).getConfigFile(), dpath)
+            shutil.copyfile(tset.getConfiguration(dir).getConfigFile(), dpath)
+            self.store.addConfig(Configuration(type, dir, "config", name, dpath))
+            
+    def deleteConfigSet(self, name, type):
+        if not self.store.hasConfigTypeStore(type):
+            raise ConfigError("type %s not valid" %type)
+        tstore = self.store.getConfigTypeStoreByName(type)
+        set = tstore.getConfigset(name)
+        for config in set.getConfigurations():
+            print "remove %s" %(config.getConfigFile())
+            os.rename(config.getConfigFile(), "%s.bak" % config.getConfigFile())
+        self.store.removeConfigSet(type, name)
+        
+    def renameConfigSet(self, name, type, newname):
+        if not self.store.hasConfigTypeStore(type):
+            raise ConfigError("type %s not valid" %type)
+        tstore = self.store.getConfigTypeStoreByName(type)
+        set = tstore.getConfigset(name)
+        for config in set.getConfigurations():
+            _oldpath=config.getConfigFile()
+            _newpath="%s/%s.%s.config.%s.xml" %(self.getConfigPath(),config.getType(), config.getDirection(), newname)
+            os.rename(_oldpath, _newpath)
+            config.setName(newname)
+            config.setConfigFile(_newpath)
+        set.setName(newname)
+        tstore.removeConfigset(name)
+        tstore.addConfigset(newname, set)
+        
+        
+    def scanConfigs(self, path = None):
+        if not path:
+            path = self.path
+        path = os.path.normpath(path)
+        dirents=dircache.listdir(path)
+        #configfile :: <type>.backup|restore.config|template|infodef.<name>.xml
+        for ent in dirents:
+            enparts=ent.split(".")
+            # must have at least 4 fields
+            if len(enparts) < 5: continue
+            # must end with .xml
+            if enparts[len(enparts)-1] != "xml": continue
+            # we  need .config files here
+            if enparts[2] != "config": continue
+            self.store.addConfig(Configuration(enparts[0], enparts[1], enparts[2], enparts[3], "%s/%s" %(path, ent)))
+
+    def scanConfigTemplates(self, path = None):
+        if not path:
+            path = self.path
+        path = os.path.normpath(path)
+        dirents=dircache.listdir(path)
+        #configfile :: <type>.backup|restore.config|template|infodef.xml
+        for ent in dirents:
+            enparts=ent.split(".")
+            # must have at least 4 fields
+            if len(enparts) < 4: continue
+            # must end with .xml
+            if enparts[len(enparts)-1] != "xml": continue
+            # we  need .config files here
+            if enparts[2] != "template": continue
+            self.store.addConfigTemplate(Configuration(enparts[0], enparts[1], enparts[2], enparts[0], "%s/%s" %(path, ent)))
+            
+    def scanConfigInfodefs(self, path = None):
+        if not path:
+            path = self.path
+        path = os.path.normpath(path)
+        dirents=dircache.listdir(path)
+        #configfile :: <type>.backup|restore.config|template|infodef.xml
+        for ent in dirents:
+            enparts=ent.split(".")
+            # must have at least 4 fields
+            if len(enparts) < 4: continue
+            # must end with .xml
+            if enparts[len(enparts)-1] != "xml": continue
+            # we  need .config files here
+            if enparts[2] != "infodef": continue
+            self.store.addConfigInfodef(Configuration(enparts[0], enparts[1], enparts[2], enparts[0], "%s/%s" %(path, ent)))
+            
+
+
+    def printConfigSets(self):
+        for store in self.store.getConfigTypeStores():
+            print store
+    
+class ConfigurationStore(object):
+    def __init__(self):
+        self.store={}
+    
+    def getConfigTypeNames(self):
+        return self.store.keys()
+    
+    def getConfigTypeStores(self):
+        return self.store.values()
+    
+    def getConfigTypeStoreByName(self, name):
+        return self.store.get(name)
+    
+    def hasConfigTypeStore(self, name):
+        return self.store.has_key(name)
+    
+    def addConfig(self, config):
+        __type = config.getType()
+        if not self.store.has_key(__type):
+            self.store[__type]=ConfigurationTypeStore(__type)
+        tstore=self.store.get(__type)
+        __name = config.getName()
+        if not tstore.hasConfigset(__name):
+            tstore.addConfigset(__name, ConfigurationSet(__name))
+        tstore.getConfigset(__name).addConfiguration(config)
+        
+    def removeConfigSet(self, type, name):
+        tstore=self.store.get(type)
+        tstore.removeConfigset(name)
+        
+
+    def addConfigTemplate(self, config):
+        __type = config.getType()
+        if not self.store.has_key(__type):
+            self.store[__type]=ConfigurationTypeStore(__type)
+        tstore=self.store.get(__type)
+        __name = config.getName()
+        tstore.getConfigTemplateset().addConfiguration(config)
+
+    def addConfigInfodef(self, config):
+        __type = config.getType()
+        if not self.store.has_key(__type):
+            self.store[__type]=ConfigurationTypeStore(__type)
+        tstore=self.store.get(__type)
+        __name = config.getName()
+        tstore.getConfigInfodefset().addConfiguration(config)
+
+            
+class ConfigurationTypeStore(object):
+    def __init__(self, type):
+        self.type=type
+        self.store={}
+        self.infodefset=ConfigurationSet(type)
+        self.templateset=ConfigurationSet(type)
+        
+    def getType(self):
+        return self.type
+    
+    def addConfigset(self, name, configset):
+        self.store[name]=configset
+        
+    def removeConfigset(self, name):
+        self.store.pop(name)
+        
+    def hasConfigset(self, name, directions=[]):
+        if not self.store.has_key(name): return False
+        return self.getConfigset(name).hasDirections(directions)
+            
+    def hasConfigTemplateset(self, directions = None):
+        return self.templateset.hasDirections(directions)
+   
+    def hasConfigInfodefset(self, directions = None):
+        return self.infodefset.hasDirections(directions)
+   
+    def getConfigsetNames(self):
+        return self.store.keys()
+    
+    def getConfigsets(self):
+        return self.store.values()
+   
+    def getConfigset(self, name):
+        return self.store.get(name)
+
+    def getConfigTemplateset(self):
+        return self.templateset
+
+    def getConfigInfodefset(self):
+        return self.infodefset
+    
+    
+    def __str__(self):
+        __str=[self.type]
+        __str.append("templateset:\t"+ self.templateset.__str__())
+        __str.append("infodefset:\t" +self.infodefset.__str__())
+        for set in self.store.values():
+             __str.append(set.__str__())
+        return "\n".join(__str)
+    
+class ConfigurationSet(object):
+    def __init__(self, name):
+        self.set={}
+        self.type=None
+        self.name=name
+        
+    def addConfiguration(self, config):   
+        self.setType(config.getType())
+        self.set[config.getDirection()] = config
+    
+    def removeConfiguration(self, direction):
+        self.set.pop(direction)
+    
+    def getConfiguration(self, direction):
+        return self.set.get(direction)
+    
+    def getConfigurations(self):
+        return self.set.values()
+    
+    def getDirections(self):
+        return self.set.keys()
+                
+    def getName(self):
+        return self.name
+    
+    def getType(self):
+        return self.type
+      
+    def hasDirection(self, direction):
+        return self.set.has_key(direction)
+    
+    def hasDirections(self, directions):
+        for dir in directions:
+            if not self.hasDirection(dir): return False
+        return True
+    
+    def setName(self, name):
+        self.name=name
+    
+    def setType(self, type):
+        if self.type:
+            if self.type != type:
+                raise TypeNotMatchError("type of %s %s and %s do not match" %(self.getName(), self.getType(), type))
+        self.type=type
+       
+    def __str__(self):
+        __str=["ConfigSet:\tname: %s\ttype:%s" %(self.name, self.type)]
+        for config in self.getConfigurations():
+            __str.append("\tconfig: %s" % str(config))
+        return "\n".join(__str)
+        
+
+                
+class Configuration(object):
+    def __init__(self, ectype, direction, filetype, name, path=None):
+        self.ectype = ectype
+        self.direction = direction
+        self.filetype=filetype
+        self.name = name
+        self.path = path
+    
+    def setConfigFile(self, path):
+        self.path=path
+        
+    def setName(self, name):
+        self.name=name
+        
+    def getConfigFile(self):
+        return self.path
+    
+    def getType(self):
+        return self.ectype
+    
+    def getDirection(self):
+        return self.direction
+ 
+    def getFiletype(self):
+        return self.filetype
+    
+    def getName(self):
+        return self.name
+    
+    def __str__(self):
+        return ("ectype: %s\tdirection: %s\tfiletype: %s\tname: %s\tpath: %s"
+                %(self.ectype, self.direction, self.filetype, self.name, self.path))
+    
+def main():
+    print "Testing ConfigurationManager"
+    manager=ConfigurationManager("/tmp/cmtest")
+    manager.scanConfigs()
+    manager.scanConfigTemplates()
+    manager.scanConfigInfodefs()
+    print "Initial ConfigStore:"
+    manager.printConfigSets()
+    manager.createConfigSet("newname", "type1")
+    print "Created new ConfigSet:"
+    manager.printConfigSets()
+    manager.deleteConfigSet("newname", "type1")
+    print "Deleted ConfigSet:"
+    manager.printConfigSets()
+    print "complete sets:"
+    print manager.getCompleteConfigSetNames()
+    
+if __name__=="__main__":
+    main()
+  
---
+++ lib/comoonics/assistant/ComConfigurationManagerTui.py
@@ -0,0 +1,155 @@
+"""
+Generic ComAssitant TUI
+"""
+
+from snack import *
+import logging
+import sys
+
+from comoonics import ComLog, odict
+from comoonics.ComExceptions import ComException
+from ComConfigurationManager import ConfigurationManager
+
+class CancelException(ComException):pass
+
+
+class ConfigurationManagerTui(object):
+    OK=0
+    NEXT=1
+    MODIFY=2
+    SAVE=3
+    RUN=4
+    CANCEL=-1
+    
+    def __init__(self, controller):
+        """
+        @param: controller, a list of AssistantControllers
+        """
+        self.controller=controller
+        self.screen = SnackScreen()
+        self.confignames=self.controller.getCompleteConfigSetNames()
+            
+    
+    def run(self, warning=None):
+        ''' @returns [name, type, direction] '''
+        #items=["a", "b"]
+        result=None
+        while True:
+            self.confignames=self.controller.getCompleteConfigSetNames()
+            names=self.confignames.keys()
+            names.sort()
+            if len(names) != 0: 
+                _res=ListboxChoiceWindow(self.screen, "Configuration Sets", "Select configuration set", names, buttons = ('Next', 'New', 'Rename', 'Delete', 'Cancel'), width = 40, scroll = 1, height = -1, default = None, help = None)
+            else:
+                _res=[ButtonChoiceWindow(self.screen, "Configuration Sets", "No Configuration sets defined", buttons = ['New', 'Cancel'], width = 40, help = None)]
+
+            if _res[0] == "cancel": break
+            if _res[0] == "new":
+                self._run_create()
+            if _res[0] == "rename":
+                _name=names[_res[1]]
+                _type=self.confignames.get(_name)
+                _newname=self._run_rename()
+                if _newname:
+                    self.controller.renameConfigSet(_name, _type, _newname)
+            if _res[0] == "delete":
+                self._run_delete(names[_res[1]])
+            if _res[0] == "next":
+                _name=names[_res[1]]
+                _type=self.confignames.get(_name)
+                set = self.controller.getConfigStore().getConfigTypeStoreByName(_type).getConfigset(_name)
+                direction = self._run_direction(_name, set.getDirections())
+                if direction == None: continue
+                result=[_name,_type,direction]
+                break
+        self.cleanup()
+        return result
+         
+    def _run_direction(self, name, directions):
+        _res=ListboxChoiceWindow(self.screen, "Mode Selection", "You selected %s\nWhat do you want to do ?" %name, directions, buttons = ('Next', 'Cancel'), width = 40, scroll = 0, height = -1, default = None, help = None)
+        if _res[0] != "next": return
+        direction = directions[_res[1]]
+        return direction
+    
+    def _run_delete(self, name):
+        if self._run_warning("This will delete configset %s" %name) == self.OK:
+            self.controller.deleteConfigSet(name, self.confignames[name])
+        
+    def _run_create(self):
+        templatetypes=self.controller.getCompleteConfigTemplateSetNames()
+        if len(templatetypes) != 0: 
+            _res=ListboxChoiceWindow(self.screen, "Configuration Types", "Select type of new configuration set", templatetypes, buttons = ('Create', 'Cancel'), width = 40, scroll = 1, height = -1, default = None, help = None)
+        else:
+            _res=[ ButtonChoiceWindow(self.screen, "Error", "No valid configurationset templates defined", buttons = [ 'Cancel' ]) ]
+        if _res[0]!="create": return 
+        type=templatetypes[_res[1]]
+        _res=EntryWindow(self.screen, "New Configuration", "Name of the new configuration set", ['Name'], allowCancel = 1, width = 40, entryWidth = 20, buttons = [ 'Ok', 'Cancel' ], help = None)
+        if _res[0]!="ok": return
+        print _res 
+        name=_res[1][0]
+        self.controller.createConfigSet(name, type)
+        
+    def _run_rename(self):
+        _res=EntryWindow(self.screen, "Rename configuration set", "Enter new name of the configuration set", ['Name'], allowCancel = 1, width = 40, entryWidth = 20, buttons = [ 'Ok', 'Cancel' ], help = None)
+        if _res[0]!="ok": return
+        name=_res[1][0]
+        return name
+    
+    def _run_warning(self, warning):
+        _rec=ButtonChoiceWindow(self.screen, "Are you sure ?", warning, buttons = [ 'Ok', 'Back' ]) 
+        if _rec == "ok":
+            return self.OK
+        else:
+            return self.CANCEL
+         
+    
+    def _exit(self, val):
+        self.cleanup()
+        raise CancelException()
+        
+        
+    def getEntryWindow(self, screen):
+        w = EntryWindow(screen ,"Com-ec Assistant", "text", ("a", "b", "c", "d", "e", "f", "g"), buttons = [ 'Ok', 'Scan', 'Cancel' ])
+        w.run()
+        
+        
+    def cleanup(self):
+        self.screen.finish()
+    
+def ButtonWindow(screen, title, text, allowCancel = 1, width = 40,
+        entryWidth = 20, buttons = [ 'Ok', 'Cancel' ], help = None):
+    """
+    EntryWindow(screen, title, text, prompts, allowCancel = 1, width = 40,
+        entryWidth = 20, buttons = [ 'Ok', 'Cancel' ], help = None):
+    """
+    bb = ButtonBar(screen, buttons);
+    t = TextboxReflowed(width, text)
+
+    g = GridFormHelp(screen, title, help, 1, 3)
+    g.add(t, 0, 0, padding = (0, 0, 0, 1))
+    g.add(bb, 0, 1, growx = 1)
+
+    result = g.runOnce()
+ 
+    return [bb.buttonPressed(result)]
+
+    
+def test():
+    ComLog.setLevel(logging.DEBUG)
+    manager=ConfigurationManager("/tmp/cmtest")
+    manager.scanConfigs()
+    manager.scanConfigTemplates()
+    manager.scanConfigInfodefs()
+    
+    tui = ConfigurationManagerTui(manager)
+    result = tui.run()
+#    at.cleanup()
+#    ac.printDocument()
+    if result==None: return None
+    set=manager.getConfigStore().getConfigTypeStoreByName(result.get("type")).getConfigset(result.get("name"))
+    infodef=manager.getConfigStore().getConfigTypeStoreByName(result.get("type")).getConfigInfodefset()
+    return [set, infodef, direction]
+    
+if __name__=="__main__":
+    print test()
+        
