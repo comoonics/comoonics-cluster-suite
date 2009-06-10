eb21bf6cfcec6267a7305fbef0248247ab43a69a
marc <marc>
2009-06-10 15:20:34 +0000

    - added debian and sles paths.

--- lib/comoonics/__init__.py
+++ lib/comoonics/__init__.py
@@ -4,6 +4,16 @@ Init this package
 
 """
 
+import sys,os.path
+# to make debian work
+if os.path.isdir('/usr/lib/python%s/site-packages/oldxml' % sys.version[:3]):
+    sys.path.insert(0, '/usr/lib/python%s/site-packages/oldxml' % sys.version[:3])
+# to make SLES and all work
+if os.path.isdir('/usr/lib/python%s/site-packages' % sys.version[:3]):
+    sys.path.append('/usr/lib/python%s/site-packages' % sys.version[:3])
+if os.path.isdir('/usr/lib64/python%s/site-packages' % sys.version[:3]):
+    sys.path.append('/usr/lib64/python%s/site-packages' % sys.version[:3])
+
 from exceptions import ImportError
 
 try:
@@ -19,7 +29,10 @@ def asConfigParser():
 
 ########
 # $Log: __init__.py,v $
-# Revision 1.3  2007-06-13 09:15:12  marc
+# Revision 1.4  2009-06-10 15:20:34  marc
+# - added debian and sles paths.
+#
+# Revision 1.3  2007/06/13 09:15:12  marc
 # - added asConfigParser
 #
 # Revision 1.2  2007/03/26 08:15:21  marc
