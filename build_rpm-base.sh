452100420f8866aa16aecae98f949a04834259bc
marc <marc>
2009-07-22 08:35:22 +0000

    build_lib2.sh based

--- build_rpm-base.sh
+++ build_rpm-base.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
-INSTALLDIR=install
+source build-lib2.sh
+
 NAME=comoonics-base-py
-CHANGELOG=$(awk '
-BEGIN { changelogfound=0; }
-/^'${NAME}'/{ changelogfound=1; next };
-/^comoonics/ { changelogfound=0; next };
-{
-  if (changelogfound == 1) {
-     print
-  }
-}
-' < docs/CHANGELOG)
 
-for file in $(find $INSTALLDIR -maxdepth 1 -type f); do
-  cp $file $(basename $file)
-done
-
-for file in $(find $INSTALLDIR/$NAME -maxdepth 1 -type f); do
-  cp $file $(basename $file)
-done
-PYTHONPATH=./ python setup.py $NAME -v bdist_rpm $@ --changelog="${CHANGELOG}"
-# --dist-dir=../../dist --bdist-base=../../build
-#popd
+build_rpms $NAME $*
