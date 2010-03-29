f55ff07efe451b8f190ec3a5dc3b1dff00906259
marc <marc>
2010-03-29 14:09:33 +0000

    - better and more tests

--- lib/comoonics/enterprisecopy/test/testComFilesystemCopyset.py
+++ lib/comoonics/enterprisecopy/test/testComFilesystemCopyset.py
@@ -8,16 +8,37 @@ import unittest
 
 class Test(unittest.TestCase):
     def setUp(self):
+        import tempfile
+        import os.path
         from comoonics.enterprisecopy.ComPathCopyObject import PathCopyObject 
         from comoonics.enterprisecopy.ComCopyObject import registerCopyObject
         registerCopyObject("path", PathCopyObject)
-        
-    def testFilesystemCopyObject1(self):
-        import tempfile
-        from comoonics.enterprisecopy.ComCopyset import Copyset
-        tempd=tempfile.mkdtemp("testFilesystemCopyObject1")
-        tempf=tempfile.mktemp("testFilesystemCopyObject1", ".tar")
-        tempm=tempfile.mktemp("testFilesystemCopyObject1", ".tar.gz")
+        self.files = [ "test1", "test2", "test3", "test4", "test5" ]
+        self.tempdirsource=tempfile.mkdtemp("destsource")
+        self.tempdirdest=tempfile.mkdtemp("destdir")
+        self.temparchivesource=tempfile.mkstemp(".tar.gz", "archivesource")
+        self.temparchivemetadata=tempfile.mkstemp(".tar", "archivemetatdata")
+        self.sourcedisk="/dev/zda"
+        self.destdisk="/dev/zdb"
+        for f in self.files:
+            _f=open(os.path.join(self.tempdirsource, f), "w")
+            _f.close()
+            
+    def tearDown(self):
+        import os.path
+        for f in self.files:
+            if os.path.exists(os.path.join(self.tempdirsource, f)):
+                os.remove(os.path.join(self.tempdirsource, f))
+            if os.path.exists(os.path.join(self.tempdirdest, f)):
+                os.remove(os.path.join(self.tempdirdest, f))
+        if os.path.exists(self.temparchivemetadata[1]):
+            os.remove(self.temparchivemetadata[1])
+        if os.path.exists(self.temparchivesource[1]):
+            os.remove(self.temparchivesource[1])
+                                       
+    def testFilesystemCopyObjectArchive1(self):
+        import tarfile
+        import os.path
         _xml="""
     <copyset type="filesystem" name="save-tmp">
         <source type="path">
@@ -34,27 +55,127 @@ class Test(unittest.TestCase):
             </data>
         </destination>
     </copyset>
-        """ %(tempd, tempm, tempf)           
-        from xml.dom.ext.reader import Sax2
-        reader=Sax2.Reader(validate=0)
-        doc=reader.fromString(_xml)
-        _copyset=Copyset(doc.documentElement, doc)
-        self.__testCopyset(_copyset)
+        """ %(self.tempdirsource, self.temparchivemetadata[1], self.temparchivesource[1])           
+        self.__testCopyset(_xml)
+        tar=tarfile.open(self.temparchivesource[1])
+        tarok=True
+        for tarinfo in tar:
+            if tarinfo.name == "./":
+                continue
+            elif os.path.basename(tarinfo.name) in self.files:
+                continue
+            else:
+                tarok=False
+        self.failIfEqual(tarok , False, "Tarfile %s does not consist of files %s." %(self.temparchivesource[1], self.files))
+        tar.close()
+
+        tar=tarfile.open(self.temparchivemetadata[1])
+        tarok=True
+        for tarinfo in tar:
+            if tarinfo.name == "./":
+                continue
+            elif os.path.basename(tarinfo.name) == "path.xml":
+                continue
+            else:
+                tarok=False
+        self.failIfEqual(tarok , False, "MetadataTarfile %s does not consist of files path.xml." %(self.temparchivemetadata[1]))
+        tar.close()
+
+    def testFilesystemCopyObjectArchive2(self):
+        import tarfile
+        import os.path
+        _xml="""
+    <copyset type="filesystem" name="save-tmp">
+        <source type="backup">
+            <metadata>
+                <archive name='%s' format='tar' type='file'>
+                    <file name='./path.xml'/>
+                </archive>
+            </metadata>
+            <data>
+                <archive name='%s' format='tar' type='file' compression='gzip'/>
+            </data>
+        </source>
+        <destination type="path">
+            <path name="%s"/>
+        </destination>
+    </copyset>
+        """ %(self.temparchivemetadata[1], self.temparchivesource[1], self.tempdirdest)
+        # Create what we need
+        self.testFilesystemCopyObjectArchive1()           
+        self.__testCopyset(_xml)
+        destok=True
+        for f in self.files:
+            if os.path.exists(os.path.join(self.tempdirdest, f)):
+                continue
+            else:
+                destok=False
+        self.failIfEqual(destok , False, "Destination %s does not consist of files %s." %(self.tempdirdest, self.files))
+
+    def testFilesystemCopyObject1(self):
+        import os.path
+        _xml="""
+    <copyset type="filesystem" name="save-tmp">
+        <source type="path">
+            <path name="%s"/>
+        </source>
+        <destination type="path">
+            <path name="%s"/>
+        </destination>
+    </copyset>
+        """ %(self.tempdirsource, self.tempdirdest)           
+        self.__testCopyset(_xml)
+        destok=True
+        for f in self.files:
+            if os.path.exists(os.path.join(self.tempdirdest, f)):
+                continue
+            else:
+                destok=False
+        self.failIfEqual(destok , False, "Destination %s does not consist of files %s." %(self.tempdirdest, self.files))
 
     def testFilesystemCopyObject2(self):
-        from comoonics.enterprisecopy.ComFilesystemCopyset import FilesystemCopyset
-        from comoonics.enterprisecopy.ComPathCopyObject import PathCopyObject
-        FilesystemCopyset.DEFAULT_OPTIONS=["--archive", "--update", "--one-file-system"]
-        copyset=FilesystemCopyset(PathCopyObject(path="/tmp", source=True), PathCopyObject(path="/tmp2", dest=True))
-        self.__testCopyset(copyset)
+        from comoonics import ComSystem
+        _xml="""
+ <copyset type="filesystem" name="save-tmp">
+    <source type="filesystem">
+      <device id="sourcerootfs" name="/dev/vg_vmware_cluster_sr/lv_sharedroot" options="skipmount">
+        <filesystem type="gfs"/>
+        <mountpoint name="/">
+          <option value="lock_nolock" name="lockproto"/>
+          <option value="hdfhgg" name="locktable"/>
+        </mountpoint>
+      </device>
+    </source>
+    <destination type="filesystem">
+      <device id="destrootfs" name="/dev/vg_vmware_cluster_srC/lv_sharedroot">
+        <filesystem clustername="vmware_cluster" type="gfs"/>
+        <mountpoint name="/var/lib/com-ec/dest">
+          <option value="lock_nolock" name="lockproto"/>
+          <option value="jhdshf" name="locktable"/>
+        </mountpoint>
+      </device>
+    </destination>
+ </copyset>
+        """          
+        oldexecmode=ComSystem.getExecMode()
+        ComSystem.setExecMode(ComSystem.SIMULATE)
+        self.__testCopyset(_xml)
+        ComSystem.setExecMode(oldexecmode)
 
-    def __testCopyset(self, _copyset):
+    def __testCopyset(self, _xml):
+        from xml.dom.ext.reader import Sax2
+        from comoonics.enterprisecopy.ComFilesystemCopyset import FilesystemCopyset
+        reader=Sax2.Reader(validate=0)
+        doc=reader.fromString(_xml)
+        _copyset=FilesystemCopyset(doc.documentElement, doc)
         try:
             _copyset.doPre()
             _copyset.doCopy()
             _copyset.doPost()
         except Exception, e:
-            self.assert_("Could not execute copyset %s" %_copyset)
+            import traceback
+            traceback.print_exc(None)
+            self.fail("Could not execute copyset %s, error: %s" %(_copyset.getAttribute("name"), e))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
---
+++ lib/comoonics/storage/test/testComFileSystem.py
@@ -0,0 +1,196 @@
+'''
+Created on Mar 25, 2010
+
+@author: marc
+'''
+import unittest
+from comoonics import ComSystem
+
+class BaseTestFileSystem(unittest.TestCase):
+    xml=None
+    filesystem=None
+    device=None
+    mountpoint=None
+    commands=None
+    simmethods=None
+    name=""
+    label="mylabel"
+
+    def setUp(self):
+        from comoonics import XmlTools
+        import StringIO
+        from comoonics.storage.ComDevice import Device
+        from comoonics.storage.ComMountpoint import MountPoint
+        from comoonics.storage.ComFileSystem import getFileSystem
+        
+        xmlfp=StringIO.StringIO(self.xml)
+        doc=XmlTools.parseXMLFP(xmlfp)
+        __device=doc.documentElement
+        
+        self.device=Device(__device, doc)
+        __fs=__device.getElementsByTagName("filesystem")[0]
+        self.filesystem=getFileSystem(__fs, doc)
+        __mp=__device.getElementsByTagName("mountpoint")[0]
+        self.mountpoint=MountPoint(__mp, doc)
+        
+    def _testMethod(self, method, execmode, *params):
+        oldmode=ComSystem.getExecMode()
+        ComSystem.clearSimCommands()
+        ComSystem.setExecMode(execmode)
+        try:
+            method(*params)
+        except Exception, e:
+            import traceback
+            traceback.print_exc()
+            self.fail("Could not execute %s method on with parameters %s, Error: %s" %(method, params, e))
+        if self.simmethods and ComSystem.isSimulate():
+            errormethods=list()
+            for simmethod in ComSystem.getSimCommands():
+                matched=False
+                for simmethod2 in self.simmethods:
+                    if not matched and isinstance(simmethod2, basestring):
+                        matched=simmethod == simmethod2
+                    elif not matched:
+                        # Must be re
+                        matched=simmethod2.match(simmethod)
+                if matched == False:
+                    errormethods.append(simmethod)
+            if len(errormethods) >0:
+                bufsimmethods=list()
+                for simmethod in self.simmethods:
+                    if isinstance(simmethod, basestring):
+                        bufsimmethods.append(simmethod)
+                    else:
+                        bufsimmethods.append(simmethod.pattern)
+                self.fail("""The commands being executed are not to be found in the commands that must have been executed.
+Commands that could not be matched are:
+%s
+Executed commands are: 
+%s
+Expected commands are:
+%s
+""" %("\n".join(errormethods), "\n".join(ComSystem.getSimCommands()), "\n".join(bufsimmethods)))
+        ComSystem.setExecMode(oldmode)
+    
+    def testMount(self):
+        self._testMethod(self.filesystem.mount, ComSystem.SIMULATE ,self.device, self.mountpoint)
+        
+    def testUmountDev(self):
+        self._testMethod(self.filesystem.umountDev, ComSystem.SIMULATE, self.device)
+        
+    def testUmountDir(self):
+        self._testMethod(self.filesystem.umountDir, ComSystem.SIMULATE, self.mountpoint)
+        
+    def testGetName(self):
+        self.failUnlessEqual(self.filesystem.getName(), self.name, "The filesystem with name %s does not equal expected name %s" %(self.filesystem.getName(), self.name))
+
+    def testFormatDevice(self):
+        self._testMethod(self.filesystem.formatDevice, ComSystem.SIMULATE, self.device)
+        
+    def testLabelDevice(self):
+        self._testMethod(self.filesystem.labelDevice, ComSystem.SIMULATE, self.device, self.label)
+        
+    def testCheckFs(self):
+        self._testMethod(self.filesystem.checkFs, ComSystem.SIMULATE, self.device)
+        
+    def testGetLabel(self):
+        self._testMethod(self.filesystem.getLabel, ComSystem.SIMULATE, self.device)
+
+class TestGfsFileSystem(BaseTestFileSystem):
+    def __init__(self, method="runTest"):
+        import re
+        self.xml="""
+  <device id="sourcerootfs" name="/dev/vg_vmware_cluster_sr/lv_sharedroot">
+    <filesystem type="gfs" journals="5" lockproto="lock_nolock" clustername="cluster1" locktable="locktable1" bsize="1024"/>
+    <mountpoint name="/">
+      <option value="lock_nolock" name="lockproto"/>
+      <option value="hdfhgg" name="locktable"/>
+    </mountpoint>
+  </device>
+"""
+        self.simmethods=["mount -t gfs -o lockproto=lock_nolock,locktable=hdfhgg /dev/vg_vmware_cluster_sr/lv_sharedroot /", 
+                         "umount /dev/vg_vmware_cluster_sr/lv_sharedroot", 
+                         "umount /",
+                         "gfs_mkfs -O  -j 5 -p lock_nolock -t cluster1:locktable1 -b 1024 /dev/vg_vmware_cluster_sr/lv_sharedroot", 
+                         "gfs_fsck -y /dev/vg_vmware_cluster_sr/lv_sharedroot",
+                         ]
+        self.name="gfs"
+        BaseTestFileSystem.__init__(self, method)
+        
+    def testGetLabel(self):
+        pass
+
+class TestExt2FileSystem(BaseTestFileSystem):
+    def __init__(self, method="runTest"):
+        import re
+        self.xml="""
+  <device id="sourcerootfs" name="/dev/vg_vmware_cluster_sr/lv_sharedroot">
+    <filesystem type="ext2"/>
+    <mountpoint name="/"/>
+  </device>
+"""
+        self.simmethods=["mount -t ext2 -o defaults /dev/vg_vmware_cluster_sr/lv_sharedroot /", 
+                         "umount /dev/vg_vmware_cluster_sr/lv_sharedroot", 
+                         "umount /",
+                         "mkfs -t ext2  /dev/vg_vmware_cluster_sr/lv_sharedroot",
+                         "e2fsck -y /dev/vg_vmware_cluster_sr/lv_sharedroot",
+                         "e2label /dev/vg_vmware_cluster_sr/lv_sharedroot mylabel",
+                         "e2label /dev/vg_vmware_cluster_sr/lv_sharedroot"
+                         ]
+        self.name="ext2"
+        BaseTestFileSystem.__init__(self, method)
+
+class TestExt3FileSystem(BaseTestFileSystem):
+    def __init__(self, method="runTest"):
+        import re
+        self.xml="""
+  <device id="sourcerootfs" name="/dev/vg_vmware_cluster_sr/lv_sharedroot">
+    <filesystem type="ext3"/>
+    <mountpoint name="/"/>
+  </device>
+"""
+        self.simmethods=["mount -t ext3 -o defaults /dev/vg_vmware_cluster_sr/lv_sharedroot /", 
+                         "umount /dev/vg_vmware_cluster_sr/lv_sharedroot", 
+                         "umount /",
+                         "mkfs -t ext3  /dev/vg_vmware_cluster_sr/lv_sharedroot",
+                         "e2fsck -y /dev/vg_vmware_cluster_sr/lv_sharedroot",
+                         "e2label /dev/vg_vmware_cluster_sr/lv_sharedroot mylabel",
+                         "e2label /dev/vg_vmware_cluster_sr/lv_sharedroot"
+                         ]
+        self.name="ext3"
+        BaseTestFileSystem.__init__(self, method)
+
+class TestOcfs2FileSystem(BaseTestFileSystem):
+    def __init__(self, method="runTest"):
+        import re
+        self.xml="""
+  <device id="sourcerootfs" name="/dev/vg_vmware_cluster_sr/lv_sharedroot">
+    <filesystem type="ocfs2"/>
+    <mountpoint name="/"/>
+  </device>
+"""
+        self.simmethods=["mount -t ocfs2 -o defaults /dev/vg_vmware_cluster_sr/lv_sharedroot /", 
+                         "umount /dev/vg_vmware_cluster_sr/lv_sharedroot", 
+                         "umount /",
+                         "yes | mkfs -t ocfs2 -F /dev/vg_vmware_cluster_sr/lv_sharedroot",
+                         "ocfs2.fsck -y /dev/vg_vmware_cluster_sr/lv_sharedroot",
+                         "tunefs.ocfs2 -L /dev/vg_vmware_cluster_sr/lv_sharedroot mylabel",
+                         "tunefs.ocfs2 -L /dev/vg_vmware_cluster_sr/lv_sharedroot"
+                         ]
+        self.name="ocfs2"
+        BaseTestFileSystem.__init__(self, method)
+
+testclasses=[TestGfsFileSystem, TestExt2FileSystem, TestExt3FileSystem, TestOcfs2FileSystem]
+testmethods=[ "testMount", "testUmountDev", "testUmountDir", "testGetName", "testFormatDevice", "testLabelDevice", "testCheckFs", "testGetLabel" ]
+if __name__ == "__main__":
+    oldmode=ComSystem.getExecMode()
+    ComSystem.setExecMode(ComSystem.SIMULATE)
+    #import sys;sys.argv = ['', 'Test.testName']
+    suite=unittest.TestSuite()
+    for testclass in testclasses:
+        for testmethod in testmethods:
+            suite.addTest(testclass(testmethod))
+
+    runner=unittest.TextTestRunner()
+    runner.run(suite)
+    ComSystem.setExecMode(oldmode)
