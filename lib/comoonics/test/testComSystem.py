05d717db502401d1962ba47b8d03677444237f03
marc <marc>
2010-04-13 13:27:48 +0000

    - new tests

--- lib/comoonics/test/testComSystem.py
+++ lib/comoonics/test/testComSystem.py
@@ -7,38 +7,72 @@ import unittest
 from comoonics import ComSystem
 
 class Test_ComSystem(unittest.TestCase):
-    cmd1='echo "hallo stdout"'
-    cmd1_result="hallo stdout\n"
-    cmd2='echo "hallo stderr" >&2'
-    cmd2_result="hallo stderr\n"
+    cmd1='echo "hallo stdout"; echo "hallo stderr" >&2'
+    cmd1_result=[ "hallo stdout\n", [ "hallo stdout\n" ], [ 0, ["hallo stdout\n"] ], [ 0, ["hallo stdout\n"], ["hallo stderr\n"] ], (0, "hallo stdout\nhallo stderr" ) ]
+    cmd1_sim_result=[ "output cmd1\n",                           # 0
+                      [ "output cmd1\n" ],                       # 1
+                      [ 0, ["output cmd1\n"] ],                  # 2
+                      [ 0, ["output cmd1\n"],["error cmd1\n"] ], # 3
+                      (0, "output cmd1\n" ) ]                    # 4
+    cmd1_simout="output cmd1\n"
+    cmd1_simerr="error cmd1\n"
 
+    def testExecLocalOutputAsString(self):
+        ComSystem.setExecMode(None)
+        result=ComSystem.execLocalOutput(self.cmd1, True, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_result[0])
     def testExecLocalOutput(self):
-        result=ComSystem.execLocalOutput(self.cmd1, True, "output cmd1 execLocalOutput")
-        self.assertEquals(result, self.cmd1_result)
-    def testExecLocalStatusOutput(self):
-        result=ComSystem.execLocalStatusOutput(self.cmd1, "output cmd1 execLocalStatusOutput")
-        self.assertEquals(result[1], self.cmd1_result[:-1])
+        ComSystem.setExecMode(None)
+        result=ComSystem.execLocalOutput(self.cmd1, False, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_result[1])
     def testExecLocalGetResult(self):
-        result=ComSystem.execLocalGetResult(self.cmd1, False, "output cmd1 execLocalGetResult")
-        self.assertEquals(result[1][0], self.cmd1_result)
-    def testExecLocalOutputErr(self):
-        result=ComSystem.execLocalOutput(self.cmd2, True, "output cmd2 execLocalOutput")
-        self.assertEquals(result, "")
-    def testExecLocalStatusOutputErr(self):
-        result=ComSystem.execLocalStatusOutput(self.cmd2, "output cmd2 execLocalStatusOutput")
-        self.assertEquals(result[1], self.cmd2_result[:-1])
+        ComSystem.setExecMode(None)
+        result=ComSystem.execLocalGetResult(self.cmd1, False, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_result[2])
     def testExecLocalGetResultErr(self):
-        result=ComSystem.execLocalGetResult(self.cmd2, True, "output cmd2 execLocalGetResult")
-        self.assertEquals(result[2][0], self.cmd2_result)
+        ComSystem.setExecMode(None)
+        result=ComSystem.execLocalGetResult(self.cmd1, True, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_result[3])
+    def testExecLocalStatusOutput(self):
+        ComSystem.setExecMode(None)
+        result=ComSystem.execLocalStatusOutput(self.cmd1, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_result[4])
     def testExecMethodErr(self):
-        result=ComSystem.execMethod(ComSystem.execLocalGetResult, self.cmd2, True)
-        self.assertEquals(result[2][0], self.cmd2_result)
+        ComSystem.setExecMode(None)
+        result=ComSystem.execMethod(ComSystem.execLocalGetResult, self.cmd1, True)
+        self.assertEquals(result, self.cmd1_result[3])
     def testExecLocalStatusOutputFalse(self):
+        ComSystem.setExecMode(None)
         result=ComSystem.execLocalStatusOutput("/bin/false", "output of /bin/false, execLocalStatusOutput")
-        self.assertTrue(result!=0)
-    def testExecLocal(self):
+        self.assertEquals(result, ( 256, "" ))
+    def testExecLocalFalse(self):
+        ComSystem.setExecMode(None)
         result=ComSystem.execLocal("/bin/false", "output of /bin/false, execLocal")
-        self.assertTrue(result!=0)
+        self.assertEquals(result, 256)
+    def testExecLocalOutputAsStringSim(self):
+        ComSystem.setExecMode(ComSystem.SIMULATE)
+        result=ComSystem.execLocalOutput(self.cmd1, True, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_sim_result[0])
+    def testExecLocalOutputSim(self):
+        ComSystem.setExecMode(ComSystem.SIMULATE)
+        result=ComSystem.execLocalOutput(self.cmd1, False, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_sim_result[1])
+    def testExecLocalGetResultSim(self):
+        ComSystem.setExecMode(ComSystem.SIMULATE)
+        result=ComSystem.execLocalGetResult(self.cmd1, False, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_sim_result[2])
+    def testExecLocalGetResultErrSim(self):
+        ComSystem.setExecMode(ComSystem.SIMULATE)
+        result=ComSystem.execLocalGetResult(self.cmd1, True, self.cmd1_simout, self.cmd1_simerr)
+        self.assertEquals(result, self.cmd1_sim_result[3])
+    def testExecLocalStatusOutputSim(self):
+        ComSystem.setExecMode(ComSystem.SIMULATE)
+        result=ComSystem.execLocalStatusOutput(self.cmd1, self.cmd1_simout)
+        self.assertEquals(result, self.cmd1_sim_result[4])
+    def testExecMethodErrSim(self):
+        ComSystem.setExecMode(ComSystem.SIMULATE)
+        result=ComSystem.execMethod(ComSystem.execLocalGetResult, self.cmd1, True)
+        self.assertEquals(result, True)
 
 def test_main():
     try:
