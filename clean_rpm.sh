829442c04ac132dc04338dbf4be99e29ef4863db
marc <marc>
2010-06-17 08:26:07 +0000

    added path

--- clean_rpm.sh
+++ clean_rpm.sh
@@ -3,5 +3,5 @@ RPMBUILDDIR=$1
 [ -z "$RPMBUILDDIR" ] && RPMBUILDDIR=$(rpmbuild --showrc | grep ": _topdir" | awk '{print $3}')
 if [ -d "$RPMBUILDDIR" ]; then
   echo "Cleaning $RPMBUILDDIR"
-  find $RPMBUILDDIR -type f -name "*.rpm" -or -name "*.spec" -or -name "*.tar.*" -delete
+  find $RPMBUILDDIR -type f \( -name "*.rpm" -or -name "*.spec" -or -name "*.tar.*" \) -delete
 fi
