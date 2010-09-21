5ba93e4f5960e176f1dc92fa3aa9ae0fb51538e2
marc <marc>
2010-09-21 14:11:29 +0000

    work with list of files containing globs

--- lib/comoonics/enterprisecopy/ComFileModification.py
+++ lib/comoonics/enterprisecopy/ComFileModification.py
@@ -7,11 +7,11 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComFileModification.py,v 1.3 2010-03-08 12:30:48 marc Exp $
+# $Id: ComFileModification.py,v 1.4 2010-09-21 14:11:29 marc Exp $
 #
 
 
-__version__ = "$Revision: 1.3 $"
+__version__ = "$Revision: 1.4 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/enterprisecopy/ComFileModification.py,v $
 
 from ComModification import Modification
@@ -21,11 +21,11 @@ class FileModification(Modification):
     """ generic FileModification Modification"""
     def __init__(self, element, doc):
         Modification.__init__(self, element, doc)
-        self.files=self.createFileList(element, doc)
         
     def doRealModifications(self):
-        for i in range(len(self.files)):
-            self.doModifications(self.files[i])
+        files=self.createFileList(self.getElement(), self.getDocument())
+        for i in range(len(files)):
+            self.doModifications(files[i])
     
     def doModifications(self, file, save=True):
         pass
@@ -33,15 +33,21 @@ class FileModification(Modification):
     # privat methods
 
     def createFileList(self, element, doc):
-        __files=list()
-        __elements=element.getElementsByTagName("file")
-        if __elements and len(__elements) > 0:
-            for i in range(len(__elements)):
-                __files.append(File(__elements[i], doc))
-        return __files
+        """ creates the filelist and globs them if necessary """
+        files=list()
+        elements=element.getElementsByTagName("file")
+        if elements and len(elements) > 0:
+            for i in range(len(elements)):
+                _elements=File.globFilename(elements[i].getAttribute(File.ATTRNAME), doc)
+                if _elements:
+                    files.extend(_elements)
+        return files
 
 # $Log: ComFileModification.py,v $
-# Revision 1.3  2010-03-08 12:30:48  marc
+# Revision 1.4  2010-09-21 14:11:29  marc
+# work with list of files containing globs
+#
+# Revision 1.3  2010/03/08 12:30:48  marc
 # version for comoonics4.6-rc1
 #
 # Revision 1.2  2010/02/09 21:48:24  mark
@@ -53,4 +59,3 @@ class FileModification(Modification):
 # Revision 1.1  2006/07/07 11:33:24  mark
 # initial release
 #
-
