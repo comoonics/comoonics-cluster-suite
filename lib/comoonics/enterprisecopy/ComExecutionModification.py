88dd80bd77bd40c39804bb1cb691c77fb18f977e
marc <marc>
2010-11-16 13:48:50 +0000

    added command as text node

--- lib/comoonics/enterprisecopy/ComExecutionModification.py
+++ lib/comoonics/enterprisecopy/ComExecutionModification.py
@@ -7,11 +7,11 @@ here should be some more information about the module, that finds its way inot t
 
 
 # here is some internal information
-# $Id: ComExecutionModification.py,v 1.3 2010-03-08 12:30:48 marc Exp $
+# $Id: ComExecutionModification.py,v 1.4 2010-11-16 13:48:50 marc Exp $
 #
 
 
-__version__ = "$Revision: 1.3 $"
+__version__ = "$Revision: 1.4 $"
 # $Source: /atix/ATIX/CVSROOT/nashead2004/management/comoonics-clustersuite/python/lib/comoonics/enterprisecopy/ComExecutionModification.py,v $
 
 from ComModification import Modification
@@ -28,7 +28,19 @@ class ExecutionModification(Modification):
         Modification.__init__(self, element, doc)
         
     def doRealModifications(self):
-        __cmd=self.getAttribute("command")
+        import xml.dom
+        import StringIO
+        if self.hasAttribute("command"):
+            __cmd=self.getAttribute("command")
+        else:
+            buf=StringIO.StringIO()
+            element=self.getElement()
+            child=element.firstChild
+            while child != None:
+                if child.nodeType==xml.dom.Node.TEXT_NODE:
+                    buf.write(child.data)
+                child=child.nextSibling
+            __cmd=buf.getvalue()
         __rc, __ret, __err = ComSystem.execLocalGetResult(__cmd, True)
         if __rc:
             raise ComException(__cmd + ":  out:" + " ".join(__ret) + \
@@ -38,7 +50,10 @@ class ExecutionModification(Modification):
                                                             " err: " + " ".join(__err))
 
 # $Log: ComExecutionModification.py,v $
-# Revision 1.3  2010-03-08 12:30:48  marc
+# Revision 1.4  2010-11-16 13:48:50  marc
+# added command as text node
+#
+# Revision 1.3  2010/03/08 12:30:48  marc
 # version for comoonics4.6-rc1
 #
 # Revision 1.2  2010/02/09 21:48:24  mark
