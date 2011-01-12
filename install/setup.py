1814b65636e001c24c4d789b9af20c4ee37c106f
marc <marc>
2011-01-12 10:16:16 +0000

    new packages

--- install/setup.py
+++ install/setup.py
@@ -6,7 +6,7 @@ distributions)."""
 
 # This module should be kept compatible with Python 2.1.
 
-__revision__ = "$Id: setup.py,v 1.11 2010-08-26 08:07:14 marc Exp $"
+__revision__ = "$Id: setup.py,v 1.12 2011-01-12 10:16:16 marc Exp $"
 
 from distutils.core import setup
 import sys, os, string
@@ -19,6 +19,305 @@ from distutils.file_util import write_file
 from distutils.errors import *
 from distutils.sysconfig import get_python_version, get_config_var
 from distutils import log
+import fnmatch
+import unittest
+import re
+
+#=======================================================================================================================
+# PydevTestRunner
+#=======================================================================================================================
+class PydevTestRunner:
+    """ finds and runs a file or directory of files as a unit test """
+
+    __py_extensions = ["*.py", "*.pyw"]
+    __exclude_files = ["__init__.*"]
+
+    def __init__(self, test_dir, test_filter=None, verbosity=2, tests=None):
+        self.test_dir = test_dir
+        self.__adjust_path()
+        self.test_filter = self.__setup_test_filter(test_filter)
+        self.verbosity = verbosity
+        self.tests = tests
+
+
+    def __adjust_path(self):
+        """ add the current file or directory to the python path """
+        path_to_append = None
+        for n in xrange(len(self.test_dir)):
+            dir_name = self.__unixify(self.test_dir[n])
+            if os.path.isdir(dir_name):
+                if not dir_name.endswith("/"):
+                    self.test_dir[n] = dir_name + "/"
+                path_to_append = os.path.normpath(dir_name)
+            elif os.path.isfile(dir_name):
+                path_to_append = os.path.dirname(dir_name)
+            else:
+                msg = ("unknown type. \n%s\nshould be file or a directory.\n" % (dir_name))
+                raise RuntimeError(msg)
+        if path_to_append is not None:
+            #Add it as the last one (so, first things are resolved against the default dirs and 
+            #if none resolves, then we try a relative import).
+            sys.path.append(path_to_append)
+        return
+
+    def __setup_test_filter(self, test_filter):
+        """ turn a filter string into a list of filter regexes """
+        if test_filter is None or len(test_filter) == 0:
+            return None
+        return [re.compile("test%s" % f) for f in test_filter]
+
+    def __is_valid_py_file(self, fname):
+        """ tests that a particular file contains the proper file extension 
+            and is not in the list of files to exclude """
+        is_valid_fname = 0
+        for invalid_fname in self.__class__.__exclude_files:
+            is_valid_fname += int(not fnmatch.fnmatch(fname, invalid_fname))
+        if_valid_ext = 0
+        for ext in self.__class__.__py_extensions:
+            if_valid_ext += int(fnmatch.fnmatch(fname, ext))
+        return is_valid_fname > 0 and if_valid_ext > 0
+
+    def __unixify(self, s):
+        """ stupid windows. converts the backslash to forwardslash for consistency """
+        return os.path.normpath(s).replace(os.sep, "/")
+
+    def __importify(self, s, dir=False):
+        """ turns directory separators into dots and removes the ".py*" extension 
+            so the string can be used as import statement """
+        if not dir:
+            dirname, fname = os.path.split(s)
+
+            if fname.count('.') > 1:
+                #if there's a file named xxx.xx.py, it is not a valid module, so, let's not load it...
+                return
+
+            imp_stmt_pieces = [dirname.replace("\\", "/").replace("/", "."), os.path.splitext(fname)[0]]
+
+            if len(imp_stmt_pieces[0]) == 0:
+                imp_stmt_pieces = imp_stmt_pieces[1:]
+
+            return ".".join(imp_stmt_pieces)
+
+        else: #handle dir
+            return s.replace("\\", "/").replace("/", ".")
+
+    def __add_files(self, pyfiles, root, files):
+        """ if files match, appends them to pyfiles. used by os.path.walk fcn """
+        for fname in files:
+            if self.__is_valid_py_file(fname):
+                name_without_base_dir = self.__unixify(os.path.join(root, fname))
+                pyfiles.append(name_without_base_dir)
+        return
+
+
+    def find_import_files(self):
+        """ return a list of files to import """
+        pyfiles = []
+
+        for base_dir in self.test_dir:
+            if os.path.isdir(base_dir):
+                if hasattr(os, 'walk'):
+                    for root, dirs, files in os.walk(base_dir):
+                        self.__add_files(pyfiles, root, files)
+                else:
+                    # jython2.1 is too old for os.walk!
+                    os.path.walk(base_dir, self.__add_files, pyfiles)
+
+            elif os.path.isfile(base_dir):
+                pyfiles.append(base_dir)
+
+        return pyfiles
+
+    def __get_module_from_str(self, modname, print_exception):
+        """ Import the module in the given import path.
+            * Returns the "final" module, so importing "coilib40.subject.visu" 
+            returns the "visu" module, not the "coilib40" as returned by __import__ """
+        try:
+            mod = __import__(modname)
+            for part in modname.split('.')[1:]:
+                mod = getattr(mod, part)
+            return mod
+        except:
+            if print_exception:
+                import traceback;traceback.print_exc()
+                sys.stderr.write('ERROR: Module: %s could not be imported.\n' % (modname,))
+            return None
+
+    def find_modules_from_files(self, pyfiles):
+        """ returns a lisst of modules given a list of files """
+        #let's make sure that the paths we want are in the pythonpath...
+        imports = [self.__importify(s) for s in pyfiles]
+
+        system_paths = []
+        for s in sys.path:
+            system_paths.append(self.__importify(s, True))
+
+
+        ret = []
+        for imp in imports:
+            if imp is None:
+                continue #can happen if a file is not a valid module
+            choices = []
+            for s in system_paths:
+                if imp.startswith(s):
+                    add = imp[len(s) + 1:]
+                    if add:
+                        choices.append(add)
+                    #sys.stdout.write(' ' + add + ' ')
+
+            if not choices:
+                sys.stdout.write('PYTHONPATH not found for file: %s\n' % imp)
+            else:
+                for i, import_str in enumerate(choices):
+                    mod = self.__get_module_from_str(import_str, print_exception=i == len(choices) - 1)
+                    if mod is not None:
+                        ret.append(mod)
+                        break
+
+
+        return ret
+
+    def find_tests_from_modules(self, modules):
+        """ returns the unittests given a list of modules """
+        loader = unittest.TestLoader()
+
+        ret = []
+        if self.tests:
+            accepted_classes = {}
+            accepted_methods = {}
+
+            for t in self.tests:
+                splitted = t.split('.')
+                if len(splitted) == 1:
+                    accepted_classes[t] = t
+
+                elif len(splitted) == 2:
+                    accepted_methods[t] = t
+
+            #===========================================================================================================
+            # GetTestCaseNames
+            #===========================================================================================================
+            class GetTestCaseNames:
+                """Yes, we need a class for that (cannot use outer context on jython 2.1)"""
+
+                def __init__(self, accepted_classes, accepted_methods):
+                    self.accepted_classes = accepted_classes
+                    self.accepted_methods = accepted_methods
+
+                def __call__(self, testCaseClass):
+                    """Return a sorted sequence of method names found within testCaseClass"""
+                    testFnNames = []
+                    className = testCaseClass.__name__
+
+                    if DictContains(self.accepted_classes, className):
+                        for attrname in dir(testCaseClass):
+                            #If a class is chosen, we select all the 'test' methods'
+                            if attrname.startswith('test') and hasattr(getattr(testCaseClass, attrname), '__call__'):
+                                testFnNames.append(attrname)
+
+                    else:
+                        for attrname in dir(testCaseClass):
+                            #If we have the class+method name, we must do a full check and have an exact match.
+                            if DictContains(self.accepted_methods, className + '.' + attrname):
+                                if hasattr(getattr(testCaseClass, attrname), '__call__'):
+                                    testFnNames.append(attrname)
+
+                    #sorted() is not available in jython 2.1
+                    testFnNames.sort()
+                    return testFnNames
+
+
+            loader.getTestCaseNames = GetTestCaseNames(accepted_classes, accepted_methods)
+
+
+        ret.extend([loader.loadTestsFromModule(m) for m in modules])
+
+        return ret
+
+
+    def filter_tests(self, test_objs):
+        """ based on a filter name, only return those tests that have
+            the test case names that match """
+        test_suite = []
+        for test_obj in test_objs:
+
+            if isinstance(test_obj, unittest.TestSuite):
+                if test_obj._tests:
+                    test_obj._tests = self.filter_tests(test_obj._tests)
+                    if test_obj._tests:
+                        test_suite.append(test_obj)
+
+            elif isinstance(test_obj, unittest.TestCase):
+                test_cases = []
+                for tc in test_objs:
+                    try:
+                        testMethodName = tc._TestCase__testMethodName
+                    except AttributeError:
+                        #changed in python 2.5
+                        testMethodName = tc._testMethodName
+
+                    if self.__match(self.test_filter, testMethodName) and self.__match_tests(self.tests, tc, testMethodName):
+                        test_cases.append(tc)
+                return test_cases
+        return test_suite
+
+
+    def __match_tests(self, tests, test_case, test_method_name):
+        if not tests:
+            return 1
+
+        for t in tests:
+            class_and_method = t.split('.')
+            if len(class_and_method) == 1:
+                #only class name
+                if class_and_method[0] == test_case.__class__.__name__:
+                    return 1
+
+            elif len(class_and_method) == 2:
+                if class_and_method[0] == test_case.__class__.__name__ and class_and_method[1] == test_method_name:
+                    return 1
+
+        return 0
+
+
+
+
+    def __match(self, filter_list, name):
+        """ returns whether a test name matches the test filter """
+        if filter_list is None:
+            return 1
+        for f in filter_list:
+            if re.match(f, name):
+                return 1
+        return 0
+
+
+    def run_tests(self):
+        """ runs all tests """
+        import sys
+        sys.stdout.write("Finding files...\n")
+        files = self.find_import_files()
+        sys.stdout.write('%s %s\n' % (self.test_dir, '... done'))
+        sys.stdout.write("Importing test modules ... ")
+        modules = self.find_modules_from_files(files)
+        sys.stdout.write("done.\n")
+        sys.stdout.write("Testfilter: %s\n" %self.test_filter)
+        sys.stdout.write("Curdir: %s\n" %os.path.realpath(os.path.curdir))
+        sys.stdout.write("Pythonpath: %s\n" %sys.path)
+        all_tests = self.find_tests_from_modules(modules)
+        if self.test_filter or self.tests:
+
+            if self.test_filter:
+                sys.stdout.write('Test Filter: %s' % ([p.pattern for p in self.test_filter],))
+
+            if self.tests:
+                sys.stdout.write('Tests to run: %s' % (self.tests,))
+
+            all_tests = self.filter_tests(all_tests)
+
+        sys.stdout.write('\n')
+        runner = unittest.TextTestRunner(stream=sys.stdout, descriptions=1, verbosity=self.verbosity)
+        return runner.run(unittest.TestSuite(all_tests))
 
 class bdist_rpm_fedora (Command):
 
@@ -424,6 +723,9 @@ class bdist_rpm_fedora (Command):
         else:
             withegginfo="%{!?withegginfo: %define withegginfo 0}"
 
+        # also test by default
+        withtest="%{!?withtest: %define withtest 1}"
+        
         spec_file = [
             '%{!?sles: %global sles 0}',
             '%if %{sles}',
@@ -433,6 +735,7 @@ class bdist_rpm_fedora (Command):
             '%endif',
             '%define modulename '+'comoonics',
             withegginfo,
+            withtest,
             '',
             'Summary: ' + self.distribution.get_description(),
             ]
@@ -550,6 +853,13 @@ class bdist_rpm_fedora (Command):
         def_build = "%s %%{name} build" %(def_setup_call)
         if self.use_rpm_opt_flags:
             def_build = 'env CFLAGS="$RPM_OPT_FLAGS" ' + def_build
+        def_test1 = "%s %%{name} test" %(def_setup_call)
+        if self.use_rpm_opt_flags:
+            def_test1 = 'env CFLAGS="$RPM_OPT_FLAGS" ' + def_test1
+        def_test="""%%if %%{withtest}
+%s
+%%endif
+""" %def_test1
 
         # insert contents of files
 
@@ -559,7 +869,7 @@ class bdist_rpm_fedora (Command):
 
         script_options = [
             ('prep', 'prep_script', "%setup -q"),
-            ('build', 'build_script', def_build),
+            ('build', 'build_script', def_build+"\n"+def_test),
             ('install', 'install_script',
              ("rm -rf $RPM_BUILD_ROOT\n"
               "%s %%{name} install "
@@ -673,6 +983,94 @@ class bdist_rpm_fedora (Command):
         return new_changelog
 
     # _format_changelog()
+
+class test (Command):
+
+    description = "build everything needed to install"
+
+    user_options = [
+        ('build-base=', 'b',
+         "base directory for build library"),
+        ('test-base=', 't',
+         "test directory for test library"),
+        ('test-filter=', 'f',
+         'which testfiles should be used.')
+        ]
+
+    boolean_options = []
+
+    help_options = [
+        ]
+
+    def initialize_options (self):
+        import os.path
+        self.build_base = os.path.join('build', 'lib')
+        self.test_base="test"
+        self.debug = None
+        self.test_filter=None
+
+    def finalize_options (self):
+        if self.test_filter and isinstance(self.test_filter, basestring):
+            self.test_filter=[ self.test_filter ]
+
+    def _build_modulepaths(self):
+        import os
+        pathnames=list()
+        if hasattr(self.distribution, "py_modules") and getattr(self.distribution, "py_modules") != None:
+            for module in self.distribution.py_modules:
+                pathname=os.sep.join(module.split(".")[:-1])
+                if not pathname in pathnames:
+                    pathnames.append(pathname)
+        if hasattr(self.distribution, "packages") and getattr(self.distribution, "packages") != None:
+            for package in self.distribution.packages:
+                pathnames.append(os.sep.join(package.split(".")))
+        return pathnames
+
+    def _get_testfiles(self):
+        import glob
+        import os.path
+        testclasses=list()
+        for modulepath in self._build_modulepaths():
+            path=os.path.join("lib", modulepath, self.test_base)
+            print "Path: %s" %path
+            for testclass in glob.glob(os.path.join(path, "test*.py")):
+                testclasses.append(testclass)
+        return testclasses
+
+    def _get_testdir(self):
+        import glob
+        import os.path
+        testdirs=list()
+        for modulepath in self._build_modulepaths():
+            path=os.path.join("lib", modulepath, self.test_base)
+            print "Path: %s" %path
+            if os.path.isdir(path):
+                testdirs.append(path)
+        return testdirs
+
+    def run (self):
+        print "test:Hello world!"
+        print "I'm: %s"%self.distribution
+        print "build_base: %s" %self.build_base
+        print "test_base: %s" %self.test_base
+        print "Modulepath: %s" %self._build_modulepaths()
+        print "Testdir: %s" %self._get_testdir()
+        print "Testclassfilter: %s" %self.test_filter
+        mypath=list()
+        mypath.append(os.path.join(os.getcwd(),self.build_base))
+        mypath.extend(sys.path)
+        sys.path=mypath
+        print "Paths: %s" %sys.path
+        testdirs=self._get_testdir()
+        if testdirs and len(testdirs)>0:
+            result=PydevTestRunner(testdirs, self.test_filter, 3, None).run_tests()
+            if len(result.errors) + len(result.failures):
+                raise IOError("Error test failed: %s" %result)
+        #for testclass in self._get_testfiles():
+        #    print "--------------------------------------"
+        #    print "Testing file %s" %testclass
+        #    execfile(testclass)
+        #    print "--------------------------------------"
 
 # Should become a command!
 def buildmanpages(setupcfg):
@@ -1032,6 +1430,6 @@ if setup_cfg.has_key(sys.argv[1]):
 elif setup_cfg.has_key(sys.argv[0]):
     basedict=setup_cfg[sys.argv[0]]
 basedict["license"]="GPLv2+"
-basedict["cmdclass"]={"bdist_rpm": bdist_rpm_fedora}
+basedict["cmdclass"]={"bdist_rpm": bdist_rpm_fedora, "test": test}
 buildmanpages(basedict)
 setup(**basedict)
