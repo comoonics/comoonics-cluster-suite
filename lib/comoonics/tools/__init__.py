ca1e512c99ee36008c9febb4221c57314b856720
Marc Grimme <grimme@atix.de>
2011-04-06 16:50:04 +0200

    added get_defaultfiles and get_defaultsenvkey to be used with commands that deal with this library.

--- lib/comoonics/tools/__init__.py
+++ lib/comoonics/tools/__init__.py
@@ -1,4 +1,13 @@
-pass
+def get_defaultsfiles():
+    import os.path
+    default_dir = "/etc/comoonics"
+    home_dir = os.path.join(os.environ['HOME'], ".comoonics")
+    globalcfgdefault_file= os.path.join(default_dir, "tools.cfg") 
+    localcfgdefault_file= os.path.join(home_dir, "tools.cfg")
+    return globalcfgdefault_file, localcfgdefault_file
+
+def get_defaultsenvkey():
+    return "COMOONICS_TOOLS_CFG" 
 
 #############
 # $Log: __init__.py,v $
