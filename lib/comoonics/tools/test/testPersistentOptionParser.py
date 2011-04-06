bc8006a1ee7e4f53fe481f494afa45a7e0fa0aed
Marc Grimme <grimme@atix.de>
2011-04-06 16:54:25 +0200

    new tests

---
+++ lib/comoonics/tools/test/testPersistentOptionParser.py
@@ -0,0 +1,54 @@
+'''
+Created on 25.03.2011
+
+@author: marc
+'''
+import unittest
+from comoonics.tools.poptparse import PersistentOptionParser, make_option
+
+class TestPersistentOptionParser(unittest.TestCase):
+
+
+    def setUp(self):
+        options=[ make_option("--param1", action="store", type="string", default="default1"),
+                  make_option("--param2", action="store_true", default=False),
+                  make_option("--param3", action="store", type="int", default=False),
+                  make_option("--param4", action="store", type="float", default=False),
+                  make_option("--param5", action="store", type="string", default=False),
+                  make_option("--param6", action="store", type="string", default="default6")]
+                 
+        self.parser=PersistentOptionParser(usage="testapp [options]", option_list=options, prog="testapp")
+        self.parser.setLocalDefaultsFilename("./testapp-local.cfg")
+        self.parser.setGlobalDefaultsFilename("./testapp.cfg")
+
+    def tearDown(self):
+        pass
+
+    def testParam1(self):
+        options, args=self.parser.parse_args([])
+#        print options.param1
+        self.assertEquals(options.param1, "defautparam1 overwritten")
+        
+    def testParam2(self):
+        options, args=self.parser.parse_args([])
+#        print options.param2
+        self.assertEquals(options.param2, True)
+        
+    def testParam3(self):
+        options, args=self.parser.parse_args([])
+#        print options.param3
+        self.assertEquals(options.param3, 4)
+        
+    def testParam4(self):
+        options, args=self.parser.parse_args([])
+#        print options.param4
+        self.assertEquals(options.param4, 4.0)
+        
+    def testParam5(self):
+        options, args=self.parser.parse_args([])
+#        print options.param5
+        self.assertEquals(options.param5, "testdir/test123")
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ lib/comoonics/tools/test/testSystemInformation.py
@@ -0,0 +1,34 @@
+'''
+Created on 24.03.2011
+
+@author: marc
+'''
+import unittest
+from comoonics.tools.ComSystemInformation import SystemInformation
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.systeminformation=SystemInformation()
+
+
+    def tearDown(self):
+        pass
+
+
+    def testgetFeatures(self):
+        self.failUnless("linux" in self.systeminformation.getFeatures(), "Systeminformation does not return linux as feature.")
+        
+    def testArchitecture(self):
+        self.failUnless(self.systeminformation.getArchitecture(), "Could not determine the architecture of this system.")
+            
+    def testName(self):
+        self.failUnless(self.systeminformation.getName(), "Could not determine the name of this system.")
+            
+    def testUptime(self):
+        self.failUnless(self.systeminformation.getUptime(), "Could not determine the uptime of this system.")
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
