8c4d396630acd28de0152ba9fc9896c41a625dc8
Marc Grimme <grimme@atix.de>
2011-04-27 19:33:47 +0200

    added get_default methods

--- lib/comoonics/analysis/__init__.py
+++ lib/comoonics/analysis/__init__.py
@@ -1,1 +1,10 @@
-pass
+def get_defaultsfiles():
+    import os.path
+    default_dir = "/etc/comoonics"
+    home_dir = os.path.join(os.environ['HOME'], ".comoonics")
+    globalcfgdefault_file= os.path.join(default_dir, "analysis.cfg") 
+    localcfgdefault_file= os.path.join(home_dir, "analysis.cfg")
+    return globalcfgdefault_file, localcfgdefault_file
+
+def get_defaultsenvkey():
+    return "COMOONICS_ANALYSIS_CFG" 
