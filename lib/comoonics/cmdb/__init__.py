07523578358a49557c2bda1c4dcbec9ae96cc1a0
Marc Grimme <grimme@atix.de>
2011-08-29 11:44:09 +0200

    get_defaultsfiles:   - fixed bug with usage of empty $HOME environment variable

--- lib/comoonics/cmdb/__init__.py
+++ lib/comoonics/cmdb/__init__.py
@@ -6,7 +6,7 @@ Init this package
 def get_defaultsfiles():
     import os.path
     default_dir = "/etc/comoonics"
-    home_dir = os.path.join(os.environ['HOME'], ".comoonics")
+    home_dir = os.path.join(os.environ.get('HOME', ''), ".comoonics")
     globalcfgdefault_file= os.path.join(default_dir, "cmdb.cfg") 
     localcfgdefault_file= os.path.join(home_dir, "cmdb.cfg")
     return globalcfgdefault_file, localcfgdefault_file
