331fb820cabd7c0b45d651d5e74879850425435e
Marc Grimme <grimme@atix.de>
2011-10-06 17:38:59 +0200

    * refactoring: made a little more flexible and reusable; reflect latest changes.

--- lib/comoonics/cdsl/test/baseSetup.py
+++ lib/comoonics/cdsl/test/baseSetup.py
@@ -21,8 +21,16 @@ print "Testpath: %s" %(testpath)
 class SetupCluster:
     def __init__(self):
         from comoonics.cluster import getClusterRepository, getClusterInfo
-        self.clusterRepository = getClusterRepository(os.path.join(testpath, "cluster.conf"))
-        self.clusterinfo = getClusterInfo(self.clusterRepository)
+        self.clusterRepositories = []
+        self.clusterInfos = []
+        self.clusterRepositories.append(getClusterRepository(os.path.join(testpath, "cluster.conf")))
+        self.clusterInfos.append(getClusterInfo(self.clusterRepositories[0]))
+        
+        self.clusterRepositories.append(None)
+        self.clusterInfos.append(None)
+        
+        self.clusterRepositories.append(getClusterRepository(maxnodeidnum=3))
+        self.clusterInfos.append(getClusterInfo(self.clusterRepositories[2]))
 
 class SetupBase(object):
     results={ "hd": (
@@ -77,51 +85,76 @@ class SetupBase(object):
                      }
 
 class SetupCDSLRepository(SetupBase):
-    def __init__(self, clusterinfo):
+    def __init__(self):
         super(SetupCDSLRepository, self).__init__()
         from comoonics.cdsl.ComCdslRepository import ComoonicsCdslRepository
         from comoonics.cdsl import getCdslRepository
         from comoonics.ComPath import Path
         import shutil
-        os.mkdir(os.path.join(tmppath, "repo2"))
-        os.mkdir(os.path.join(tmppath, "repo2/repo3"))
+        os.mkdir(os.path.join(tmppath, "repo1"))
+        os.mkdir(os.path.join(tmppath, "repo1/repo2"))
+        os.mkdir(os.path.join(tmppath, "repo1/repo2/repo3"))
         os.mkdir(os.path.join(tmppath, "repo4"))
         os.mkdir(os.path.join(tmppath, "repo4/repo5"))
         os.mkdir(os.path.join(tmppath, "repo4/repo5/repo6"))
         os.mkdir(os.path.join(tmppath, "repo7"))
         os.mkdir(os.path.join(tmppath, "repo8"))
+        os.mkdir(os.path.join(tmppath, "repo8/repo9"))
+        os.mkdir(os.path.join(tmppath, "repo8/repo9/repo10"))
+        
         # Need for testing migration!!
+        # Will be created inside test not here!!
         os.makedirs(os.path.join(tmppath, "repo7", "var/lib/cdsl"))
         shutil.copyfile(os.path.join(testpath, "cdsl4.xml"), os.path.join(tmppath, "repo7", ComoonicsCdslRepository.default_resources[1]))
         
         wpath=Path()
         wpath.pushd(tmppath)
-        self.cdslRepository1 = getCdslRepository(clusterinfo=clusterinfo, root=tmppath, usenodeids="True")
-        self.cdslRepository2 = getCdslRepository(clusterinfo=clusterinfo, root=tmppath, mountpoint="repo2", usenodeids="True")
-        self.cdslRepository1.addRepository(self.cdslRepository2)
-        self.cdslRepository3 = getCdslRepository(clusterinfo=clusterinfo, root=os.path.join(tmppath,"repo2"), mountpoint="repo3", usenodeids="True")
-        self.cdslRepository2.addRepository(self.cdslRepository3)
+        self.cdslRepositories= []
         
-        self.cdslRepository4 = getCdslRepository(root=tmppath, mountpoint="repo4", usenodeids="True", maxnodeidnum="4")
-        self.cdslRepository5 = getCdslRepository(root=os.path.join(tmppath, "repo4"), mountpoint="repo5", usenodeids="True", maxnodeidnum="4")
-        self.cdslRepository4.addRepository(self.cdslRepository5)
-        self.cdslRepository6 = getCdslRepository(root=os.path.join(tmppath, "repo4", "repo5"), mountpoint="repo6", usenodeids="True", maxnodeidnum="4")
-        self.cdslRepository5.addRepository(self.cdslRepository6)
+        repopath=os.path.join(tmppath, "repo1")
+        cdslRepository1 = getCdslRepository(root=repopath, usenodeids="True")
+        cdslRepository2 = getCdslRepository(root=repopath, mountpoint="repo2", usenodeids="True")
+        cdslRepository1.addRepository(cdslRepository2)
+        cdslRepository3 = getCdslRepository(root=os.path.join(repopath,"repo2"), mountpoint="repo3", usenodeids="True")
+        cdslRepository2.addRepository(cdslRepository3)
+        self.cdslRepositories.append(cdslRepository1)
         
+        repopath=os.path.join(tmppath, "repo4")
+        cdslRepository1 = getCdslRepository(root=repopath, mountpoint="", usenodeids="True", maxnodeidnum="4")
+        cdslRepository2 = getCdslRepository(root=repopath, mountpoint="repo5", usenodeids="True", maxnodeidnum="4")
+        cdslRepository1.addRepository(cdslRepository2)
+        cdslRepository3 = getCdslRepository(root=os.path.join(repopath, "repo5"), mountpoint="repo6", usenodeids="True", maxnodeidnum="4")
+        cdslRepository2.addRepository(cdslRepository3)
+        self.cdslRepositories.append(cdslRepository1)
+
+        repopath=os.path.join(tmppath, "repo8")
+        cdslRepository1 = getCdslRepository(root=repopath, usenodeids="True")
+        cdslRepository2 = getCdslRepository(root=repopath, mountpoint="repo9", usenodeids="True")
+        cdslRepository1.addRepository(cdslRepository2)
+        cdslRepository3 = getCdslRepository(root=os.path.join(repopath,"repo9"), mountpoint="repo10", usenodeids="True")
+        cdslRepository2.addRepository(cdslRepository3)
+        self.cdslRepositories.append(cdslRepository1)
+
         wpath.popd()
         #self.cdslRepository7 = ComoonicsCdslRepository(root=os.path.join(tmppath, "repo7"))
 
     def cleanUpInfrastructure(self, path):
-        import shutil
-        from comoonics.cdsl.ComCdslRepository import ComoonicsCdslRepository
-        os.rmdir(os.path.join(self.cdslRepository6.root, self.cdslRepository6.getMountpoint())) 
-        os.rmdir(os.path.join(self.cdslRepository5.root, self.cdslRepository5.getMountpoint())) 
-        os.rmdir(os.path.join(self.cdslRepository4.root, self.cdslRepository4.getMountpoint())) 
-        os.rmdir(os.path.join(self.cdslRepository3.root, self.cdslRepository3.getMountpoint())) 
-        os.rmdir(os.path.join(self.cdslRepository2.root, self.cdslRepository2.getMountpoint()))
-        os.remove(os.path.join(tmppath, "repo7", ComoonicsCdslRepository.default_resources[1]))
-        shutil.rmtree(os.path.join(tmppath, "repo7"))
-        os.rmdir(os.path.join(tmppath, "repo8"))
+        for repo in self.cdslRepositories:
+            self.cleanUpRepo(repo)
+            os.rmdir(os.path.join(repo.root, repo.getMountpoint()))
+            
+        # now repo7
+        os.remove(os.path.join(tmppath, "repo7", "var", "lib", "cdsl", "cdsl_inventory.xml"))
+        os.rmdir(os.path.join(tmppath, "repo7", "var", "lib", "cdsl"))
+        os.rmdir(os.path.join(tmppath, "repo7", "var", "lib"))
+        os.rmdir(os.path.join(tmppath, "repo7", "var"))
+        os.rmdir(os.path.join(tmppath, "repo7"))
+    
+    def cleanUpRepo(self, repo):
+        if repo.getRepositories():
+            for childrepo in repo.getRepositories().values():
+                self.cleanUpRepo(childrepo)
+                os.rmdir(os.path.join(childrepo.root, childrepo.getMountpoint()))
             
 class SetupCDSLs(SetupBase):
     def __init__(self, repository, mynodeid="1"):
@@ -139,20 +172,8 @@ class SetupCDSLs(SetupBase):
         self.mynodeid=mynodeid
         # cdslname: { expanded, HD/Shareed, sourcepaths, destpaths, subpaths
     
-    def _createCDSLFiles(self, _tmppath):
-        from comoonics.cdsl import cmpbysubdirs
-        _cdsls=self.results.keys()
-        _cdsls.sort(cmpbysubdirs)
-        for _cdsl in _cdsls:
-            if _cdsl.endswith("d") and not os.path.exists(os.path.join(_tmppath, _cdsl)):
-                os.makedirs(os.path.join(_tmppath, _cdsl))
-            elif not os.path.exists(os.path.join(_tmppath, _cdsl)):
-                open(os.path.join(_tmppath, _cdsl), "w+")
-
-    def setupCDSLInfrastructure(self, path, cdslRepository, clusterinfo):
+    def _createRepository(self, clusterinfo):
         from comoonics.cdsl import getCdsl, CdslNotFoundException, CDSL_HOSTDEPENDENT_TYPE, CDSL_SHARED_TYPE
-        from comoonics.ComPath import Path
-        self.repository.buildInfrastructure(clusterinfo)
         _dirs=self.results.keys()
         _dirs.sort()
         for _path in _dirs:
@@ -167,7 +188,21 @@ class SetupCDSLs(SetupBase):
             
             if _cdsl:
                 self.repository.commit(_cdsl)
-        
+    
+    def _createCDSLFiles(self, _tmppath):
+        from comoonics.cdsl import cmpbysubdirs
+        _cdsls=self.results.keys()
+        _cdsls.sort(cmpbysubdirs)
+        for _cdsl in _cdsls:
+            if _cdsl.endswith("d") and not os.path.exists(os.path.join(_tmppath, _cdsl)):
+                os.makedirs(os.path.join(_tmppath, _cdsl))
+            elif not os.path.exists(os.path.join(_tmppath, _cdsl)):
+                open(os.path.join(_tmppath, _cdsl), "w+")
+
+    def setupCDSLInfrastructure(self, path, cdslRepository, clusterinfo):
+        from comoonics.ComPath import Path
+        self.repository.buildInfrastructure(clusterinfo)
+        self._createRepository(clusterinfo)
         wpath=Path()
         wpath.pushd(self.repository.workingdir)
         if os.path.isdir(cdslRepository.getLinkPath()):
@@ -192,6 +227,7 @@ class SetupCDSLs(SetupBase):
 from unittest import TestProgram, TextTestRunner
 
 class MyTestProgram(TestProgram):
+    repository=None
     def runTests(self):
         if self.testRunner is None:
             self.testRunner = TextTestRunner(verbosity=self.verbosity)
