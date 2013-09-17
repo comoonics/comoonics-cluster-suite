03ed49a7b012255e44368068c64a69c7f082a1f4
Marc Grimme <grimme@atix.de>
2013-09-17 11:12:44 +0200

    Being able to import Legato and Sepsesam differently

--- lib/comoonics/backup/__init__.py
+++ lib/comoonics/backup/__init__.py
@@ -6,6 +6,10 @@ Init this package
 from exceptions import ImportError
 try:
    import comoonics.backup.EMCLegato
+except ImportError:
+#    print "Could not import comoonics.backup.EMCLegato"
+   pass
+try:
    import comoonics.backup.SepSesam
 except ImportError:
 #    print "Could not import comoonics.backup.EMCLegato"
